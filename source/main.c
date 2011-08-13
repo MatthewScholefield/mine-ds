c8322d5cb32f5b1da4730a5645cb9b24219c309d
mariostudios@hotmail.de <mariostudios@hotmail.de@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-08-13 22:16:08 +0000

    Startscreen images now finished and implemented into the game. Have to convert the BMP's to PNG >_>

--- source/main.c
+++ source/main.c
@@ -2,16 +2,25 @@
 #include <stdio.h>
 
 #include "startscreenbg.h"
+#include "homebottom.h"
 
 int main(void)
 {
 	videoSetMode(MODE_5_2D);
     vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 
-	int bg3 = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+	int bg = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
 
-	dmaCopy(startscreenbgBitmap, bgGetGfxPtr(bg3), 256*256);
+	dmaCopy(startscreenbgBitmap, bgGetGfxPtr(bg), 256*256);
 	dmaCopy(startscreenbgPal, BG_PALETTE, 256*2);
+	
+	videoSetModeSub(MODE_5_2D);
+    vramSetBankC(VRAM_C_SUB_BG_0x06200000);
+
+	int bg3 = bgInitSub(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
+
+	dmaCopy(homebottomBitmap, bgGetGfxPtr(bg3), 256*256);
+	dmaCopy(homebottomPal, BG_PALETTE_SUB, 256*2);
 
 	while(1) {
 		swiWaitForVBlank();
