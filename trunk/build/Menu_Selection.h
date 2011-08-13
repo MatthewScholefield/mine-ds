8d5267e58fb9638d3666ab74a580f64707ab7c41
mariostudios@hotmail.de <mariostudios@hotmail.de@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-08-13 20:38:15 +0000

    Removed useless shit and added grit files

---
+++ trunk/build/Menu_Selection.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(Menu_Selection)
+
+//======================================================================
+//
+//	Menu_Selection, 255x191@8, 
+//	+ palette 256 entries, not compressed
+//	+ bitmap not compressed
+//	Total size: 512 + 48896 = 49408
+//
+//	Time-stamp: 2011-08-13, 22:21:50
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.8
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_MENU_SELECTION_H
+#define GRIT_MENU_SELECTION_H
+
+#define Menu_SelectionBitmapLen 48896
+extern const unsigned int Menu_SelectionBitmap[12224];
+
+#define Menu_SelectionPalLen 512
+extern const unsigned short Menu_SelectionPal[256];
+
+#endif // GRIT_MENU_SELECTION_H
+
+//}}BLOCK(Menu_Selection)
---
+++ trunk/build/startscreenbg.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(startscreenbg)
+
+//======================================================================
+//
+//	startscreenbg, 256x192@8, 
+//	+ palette 256 entries, not compressed
+//	+ bitmap not compressed
+//	Total size: 512 + 49152 = 49664
+//
+//	Time-stamp: 2011-08-13, 22:21:51
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.8
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_STARTSCREENBG_H
+#define GRIT_STARTSCREENBG_H
+
+#define startscreenbgBitmapLen 49152
+extern const unsigned int startscreenbgBitmap[12288];
+
+#define startscreenbgPalLen 512
+extern const unsigned short startscreenbgPal[256];
+
+#endif // GRIT_STARTSCREENBG_H
+
+//}}BLOCK(startscreenbg)
