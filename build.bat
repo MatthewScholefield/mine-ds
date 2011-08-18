8ad6655f1fc72c990311903d1e959c71d94059fe
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-08-18 08:48:35 +0000

    Now this project kinda works! You must run build.bat to copy some files needed for compilation

---
+++ build.bat
@@ -0,0 +1,7 @@
+mkdir gfxx
+cp gfx/startscreenbg.png gfxx/
+cp gfx/homebottom.png gfxx/
+cp gfx/background.grit gfxx/
+cp gfxx/background.grit gfxx/startscreenbg.grit
+cp gfxx/background.grit gfxx/homebottom.grit
+make
