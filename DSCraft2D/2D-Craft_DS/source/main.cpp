d15a0d80b063a4d4ec875d2976bbe906126157c0
mariostudios@hotmail.de <mariostudios@hotmail.de@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-11 16:26:12 +0000

    Fixed "Go up and down at same place" :D

--- DSCraft2D/2D-Craft_DS/source/main.cpp
+++ DSCraft2D/2D-Craft_DS/source/main.cpp
@@ -1,197 +1,110 @@
 #include <nds.h>
-
-#include "TitleBottom.h"
-#include "TitleTop.h"
-#include "Block1.h"
-#include "Block2.h"
-#include "Block3.h"
+#include "gameshelper.h"
+#include "blocks.h"
 #include "PlayerR.h"
-#include "PlayerL.h"
+int x,y,vy;
+int gravity=1;
+int blockposx[8];
+int blockposy[8];
+int blocktype[8];
+	int framecount=0;
+void updategravity(){
+	if (vy<20) vy+=gravity; //vy is speed
+	bool dofall=true;
+	int i;
+	scanKeys();
+	for(i=0;i<=7;i++)
+	{
+		if(spritecol(x,y,blockposx[i],blockposy[i],32,64,32,32) && blocktype[i]==0)
+		{
+			dofall=false;
+			y=blockposy[i]-63;
+			vy=0;		
+		}
+		else if (spritecol(x,y,blockposx[i],blockposy[i],32,64,32,32) && blocktype[i]==1)
+		{
+			vy=-17;
+		}
+	}
+	if (dofall)
+	{
+			y+=vy;
+	}
+	else if(keysHeld() & KEY_A)
+	{
+		//Make our speed a negitive number, this will make the guy go up!
+		vy=-12; // The -value is the rate which the guy jumps (DONT make it 1 hundred :P OR 1)
+	}
 
-int count;
-int e = 0;
-int Spr1X,Spr2X,Spr1Y,Spr2Y,Spr3X,Spr3Y,PlX,PlY;
-int FallSpeed = 0;
-bool falling = false;
-
-void timer()
-{
-	    scanKeys();
-		int held = keysHeld();
- 
-		if( held & KEY_LEFT)
-		PlX = PlX -1;
- 
-		if( held & KEY_RIGHT)
-		PlX = PlX +1;
-		
-		int i;
-		
-		if( held & KEY_UP)
-		{
-		  for (i = 0; i < 16; i++)
-		  {
-		   PlY = PlY -1;
-		  }
-		}
-		
-		if (PlY == 256)
-		{
-		PlY = 0;
-		}
-			
-        if(PlX >= Spr1X && PlX <= Spr1X+16)
-         {
-           if(PlY >= Spr1Y && PlY <= Spr1Y+16)
-            {
-             falling = false;
-			 FallSpeed == 0;
-            }
-         }
-		else
-		{
-		  falling = true;
-		  PlY = PlY +FallSpeed;
-		  if (FallSpeed == 10)
-		  {
-		  
-		  }
-		  else
-		  {
-		    FallSpeed = FallSpeed +1;
-		  }
-		}
-		
+	if(keysHeld() & KEY_LEFT)
+		x-=2;
+	if (keysHeld() & KEY_RIGHT)
+		x+=2;
 }
-
-
-int main(void)
+void setupOam(){
+	oamInit(&oamMain,SpriteMapping_1D_32,true); //Main oam With extended palettes
+}
+void copypalettes()
 {
-
-   	vramSetBankB(VRAM_B_MAIN_BG_0x06000000);
-
-        int bg = bgInit(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
-
-        dmaCopy(TitleTopBitmap, bgGetGfxPtr(bg), 256*256);
-        dmaCopy(TitleTopPal, BG_PALETTE, 256*2);
-  
-  
-    videoSetModeSub(MODE_5_2D);
-   	vramSetBankC(VRAM_C_SUB_BG_0x06200000);
-
-        int bg3 = bgInitSub(3, BgType_Bmp8, BgSize_B8_256x256, 0,0);
-
-        dmaCopy(TitleBottomBitmap, bgGetGfxPtr(bg3), 256*256);
-        dmaCopy(TitleBottomPal, BG_PALETTE_SUB, 256*2);
-
-
-  videoSetMode(MODE_5_2D);
-  vramSetBankA(VRAM_A_MAIN_SPRITE);
-  oamInit(&oamMain,SpriteMapping_1D_32,true);
-  vramSetBankF(VRAM_F_LCD);
-  
-  int i;
-  
-  for (i=0;i<Block1PalLen;i++)
-  VRAM_F_EXT_SPR_PALETTE[0][i] = Block1Pal[i];
-  for (i=0;i<=Block2PalLen;i++)
-  VRAM_F_EXT_SPR_PALETTE[1][i] = Block2Pal[i];
-  for (i=0;i<=Block3PalLen;i++)
-  VRAM_F_EXT_SPR_PALETTE[2][i] = Block3Pal[i];
-  for (i=0;i<=PlayerRPalLen;i++)
-  VRAM_F_EXT_SPR_PALETTE[3][i] = PlayerRPal[i];
-  for (i=0;i<=PlayerRPalLen;i++)
-  VRAM_F_EXT_SPR_PALETTE[4][i] = PlayerLPal[i];
-	
-  vramSetBankF(VRAM_F_SPRITE_EXT_PALETTE);
-  
-  u16* Block1 = oamAllocateGfx(&oamMain, SpriteSize_16x16, SpriteColorFormat_256Color);
-  u16* Block2 = oamAllocateGfx(&oamMain, SpriteSize_16x16, SpriteColorFormat_256Color);
-  u16* Block3 = oamAllocateGfx(&oamMain, SpriteSize_16x16, SpriteColorFormat_256Color);
-  u16* PlayerR = oamAllocateGfx(&oamMain, SpriteSize_32x64, SpriteColorFormat_256Color);
-  u16* PlayerL = oamAllocateGfx(&oamMain, SpriteSize_32x64, SpriteColorFormat_256Color);
-  
-  dmaCopy(Block1Tiles, Block1, Block1TilesLen);
-  dmaCopy(Block2Tiles, Block2, Block2TilesLen);
-  dmaCopy(Block3Tiles, Block3, Block3TilesLen);
-  dmaCopy(PlayerRTiles, PlayerR, PlayerRTilesLen);
-  dmaCopy(PlayerLTiles, PlayerL, PlayerLTilesLen);
-  
-  Spr1X = 32;
-  Spr1Y = 128;
-  Spr2X = 48;
-  Spr2Y = 128;
-  Spr3X = 64;
-  Spr3Y = 128;
-  PlX = 32;
-  PlY = 64;
-  
-  timerStart(0, ClockDivider_256, TIMER_FREQ(490), timer);
-  
-  lcdMainOnBottom();
-  
-  while(1)
-  {
-    oamSet(&oamMain,
-	        0, //SpriteNumber (0-127)
-			Spr1X, Spr1Y, //Sprite X and Y variables)
-			0, //Priority (0 render last)
-			0, //Palete number if Multiple Paletes
-			SpriteSize_16x16,
-			SpriteColorFormat_256Color,
-			Block1, //Pointer to the graphic
-			-1, //Sprite rotation data
-			false, //double the size when rotating?
-			false, //hide the sprite?
-			false, false, //vflip?, hflip?
-			false //apply mosaic?
-			);
-	oamSet(&oamMain,
-	        1,
-			Spr2X, Spr2Y,
-			0,
-			1,
-			SpriteSize_16x16,
-			SpriteColorFormat_256Color,
-			Block2,
-			-1,
-			false,
-			false,
-			false, false,
-			false
-			);
-	oamSet(&oamMain,
-	        2,
-			Spr3X, Spr3Y,
-			0,
-			2,
-			SpriteSize_16x16,
-			SpriteColorFormat_256Color,
-			Block3,
-			-1,
-			false,
-			false,
-			false, false,
-			false
-			);
-	oamSet(&oamMain,
-	        3,
-			PlX, PlY,
-			0,
-			3,
-			SpriteSize_32x64,
-			SpriteColorFormat_256Color,
-			PlayerR,
-			-1,
-			false,
-			false,
-			false, false,
-			false
-			);
-			
-			
+dmaCopy(blocksPal,VRAM_F_EXT_SPR_PALETTE[0],blocksPalLen); //A Quick way of doing a for loop copy
+dmaCopy(PlayerRPal,VRAM_F_EXT_SPR_PALETTE[1],PlayerRPalLen); //A Quick way of doing a for loop copy
+}
+int main(){
+	videoSetMode(MODE_5_2D);
+	setupOam();
+	vramSetBankA(VRAM_A_MAIN_SPRITE);//Make room for Sprites THIS USES A
+	vramSetBankF(VRAM_F_LCD);//Unlock VRAM_F
+	copypalettes();
+  	vramSetBankF(VRAM_F_SPRITE_EXT_PALETTE);//Then SEt F
+	u16* blocks[3]; //4 sprite gfx spaces in a array 
+	int i;
+	//Allocate memory
+	for (i=0;i<=3;i++)
+		blocks[i]=oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
+	u16* player=oamAllocateGfx(&oamMain, SpriteSize_32x64, SpriteColorFormat_256Color);	
+	dmaCopy(PlayerRTiles,player,PlayerRTilesLen);//Copy the player
+//	----------------------IMPORTANT CODE BELOW---------------------------
+	u8* tile=(u8*)&blocksTiles;
+	dmaCopy(blocksTiles,blocks[0],32*32); //Notice the 32*32, Copy only the first 32x32 pixels
+						//In the grit file there is a -gB8 line which means 8 bits per pixel this is one byte
+						// 32x32 pixels is the same as 32*32 bytes
+	tile+=32*32;
+	dmaCopy(tile,blocks[1],32*32);//I'm moving the pointer forward 32*32 pixels as well!
+	tile+=32*32;
+	dmaCopy(tile,blocks[2],32*32);
+//	--------------------------END OF IMPORTANT CODE----------------------
+	//Set the positions of the blocks
+	for (i=0;i<=7;i++)
+		blockposy[i]=128;
+	//Set the type of the blocks to all 0 (non bouncy)
+	for (i=0;i<=7;i++)
+		blocktype[i]=0;
+	//Make the last block bouncy
+	blocktype[7]=1;
+	blocktype[2]=2; //make the 3rd block walk through
+	blocktype[3]=2; //make the 4ht block walk through
+	//Set the x position of the blocks
+	blockposx[0]=0;
+	blockposx[1]=32;
+	blockposx[2]=64;
+	blockposx[3]=96;
+	blockposx[4]=128;
+	blockposx[5]=160;
+	blockposx[6]=192;
+	blockposx[7]=224;
 	swiWaitForVBlank();
-	oamUpdate(&oamMain);
-   }
+
+	while(1)
+	{
+		updategravity(); 
+		for(i=0;i<=7;i++)//Draw the blocks
+			oamSet(&oamMain,i,blockposx[i],blockposy[i],0,0,SpriteSize_32x32,SpriteColorFormat_256Color,blocks[i%3],-1,false,false,false,false,false);
+		//Now Draw the Player
+		oamSet(&oamMain,8,x,y,0,1,SpriteSize_32x64,SpriteColorFormat_256Color,player,-1,false,y<-64,keysHeld() & KEY_LEFT,false,false); //The keysHeld() & KEY_LEFT Flips the image on the H axis if you press left :) The y<0 part hides the sprite if he jumps too high :)
+		swiWaitForVBlank();
+		oamUpdate(&oamMain);
+		if(y>192) y=0;
+		if(x>256)x=0;
+		if (x<-32)x=256;
+	}
 }
-		
