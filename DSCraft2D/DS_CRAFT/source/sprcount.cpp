d01a04e8149807a9b0c9366c36e265845bd13e9f
CoolAs <goldenscruff@gmail.com>
2011-10-14 09:36:42 +0000

    Comments, no REAL progress.

--- DSCraft2D/DS_CRAFT/source/sprcount.cpp
+++ DSCraft2D/DS_CRAFT/source/sprcount.cpp
@@ -1,3 +1,7 @@
+//This Code keeps track of the sprites that are displayed on the screen, 
+//It gives every sprite a different oamId and makes sure that there are no two sprites with the same oamId
+//nextSprite(); gives the next oamId and returns it
+//resetSpriteCount(); needs to be done once per frame and sets the oamID count to 0
 int sprite;
 void resetSpriteCount(){
 	sprite=0;
