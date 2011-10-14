d6714999b9c6b1401ddd5c15a5b0503bb05b6aba
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-14 09:36:42 +0000

    Comments, no REAL progress.

--- trunk/DSCraft2D/DS_CRAFT/source/main.cpp
+++ trunk/DSCraft2D/DS_CRAFT/source/main.cpp
@@ -1,14 +1,14 @@
 #include <nds.h>
-#include "ndsvideo.h"
-#include "player.h"
-#include "sprcount.h"
+#include "ndsvideo.h" // Include the video difanations (for setupVideo();)
+#include "player.h" // The player fuctions
+#include "sprcount.h" //The sprite number counter
 int main(){
-	setupVideo(); //Setup all the video we need
-	playerActor MainPlayer;
+	setupVideo(); //Setup all the video we need (in ndsvideo.h/cpp)
+	playerActor MainPlayer; //Create a Player Object
 	while(1){
-		updateplayer(&MainPlayer);	
-		swiWaitForVBlank();
-		oamUpdate(&oamMain);
-		resetSpriteCount();
+		updateplayer(&MainPlayer);	//Update the player
+		swiWaitForVBlank(); //Wait for a VBlank
+		oamUpdate(&oamMain); //Update the sprites
+		resetSpriteCount(); //And set the sprite number counter to 0
 	}
 }
