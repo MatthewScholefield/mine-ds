40d767e58915ea8b7282bc453f7229843305c62a
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-15 21:52:15 +0000

    jaggered arrays and the world gen bug fixed

--- DSCraft2D/DS_CRAFT/source/world.cpp
+++ DSCraft2D/DS_CRAFT/source/world.cpp
@@ -7,9 +7,9 @@ int i,j;
 void worldUpdate(worldObject* world){
 	for (i=0;i<=32;i++){//Cycle through the block array
 		for (j=0;j<=32;j++){
-			if (world->blocks[i]==STONE) STONE_render(i*32-world->CamX,j*32-world->CamY);	
-			else if (world->blocks[i]==GRASS) GRASS_render(i*32-world->CamX,j*32-world->CamY);	
-			else if (world->blocks[i]==DIRT) DIRT_render(i*32-world->CamX,j*32-world->CamY);	
+			if (world->blocks[i][j]==STONE) STONE_render(i*32-world->CamX,j*32-world->CamY);	
+			else if (world->blocks[i][j]==GRASS) GRASS_render(i*32-world->CamX,j*32-world->CamY);	
+			else if (world->blocks[i][j]==DIRT) DIRT_render(i*32-world->CamX,j*32-world->CamY);	
 		}	
 	}
 }
--- DSCraft2D/DS_CRAFT/source/world.h
+++ DSCraft2D/DS_CRAFT/source/world.h
@@ -1,7 +1,7 @@
 //Circular Dependances RUBBISH (google it(why isn't google a verb))
 #ifndef WORLD_H
 typedef struct{
-	int blocks[32*32]; //A world will have 32 by 32 blocks, for now as for testing
+	int blocks[32][32]; //A world will have 32 by 32 blocks, for now as for testing
 	int CamY;
 	int CamX;
 }worldObject;
