a82fc1e12bb3a598bdb4d56e2f5054df36b36eb9
mariostudios@hotmail.de <mariostudios@hotmail.de@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-16 01:02:48 +0000

    Added leafs :) UNDERSTAND NOW THE CODE!!!

--- DSCraft2D/DS_CRAFT/source/main.cpp
+++ DSCraft2D/DS_CRAFT/source/main.cpp
@@ -21,7 +21,7 @@ int main(){
 	int i,j;
 	for(i=0;i<=32;i++){
 		for(j=0;j<=32;j++){
-			CurrentWorld.blocks[i][j]=rand()%11;//11 is the amount of blocks implemented
+			CurrentWorld.blocks[i][j]=rand()%15;//11 is the amount of blocks implemented
 	}}
 	worldSetUp();
 	while(1){
