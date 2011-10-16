bc0503a355efb658d20033347359f532494bb355
CoolAs <goldenscruff@gmail.com>
2011-10-16 01:06:17 +0000

    Added logs

---
+++ DSCraft2D/DS_CRAFT/source/blocks/log.cpp
@@ -0,0 +1,15 @@
+#include <nds.h> //BLAH
+#include "../ndsvideo.h"
+#include "block.h" //Include the block graphics
+#include "../blockID.h"
+u16* LOGgfx;
+void LOG_render(int x,int y){
+	if (y<192 && x<256 && x>(-32) && y>(-32))createsprite32x32(x,y,LOGgfx,false,0);	
+}
+void LOG_setup(){
+	LOGgfx=oamAllocateGfx(&oamMain,SpriteSize_32x32,SpriteColorFormat_256Color);
+	char* blocktiles;
+	blocktiles=(char*)&blockTiles;
+	blocktiles+=(32*32)*LOG;
+	dmaCopy(blocktiles,LOGgfx,32*32);
+}
---
+++ DSCraft2D/DS_CRAFT/source/blocks/log.h
@@ -0,0 +1,4 @@
+//A block has Two Functions
+void LOG_render(int a, int b); //Render the block
+void LOG_setup(); //And copy the blocks graphics
+
