a1f2a16216fbbc3a07ad46b8741bf6f3658906cd
mariostudios@hotmail.de <mariostudios@hotmail.de@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-16 01:02:48 +0000

    Added leafs :) UNDERSTAND NOW THE CODE!!!

---
+++ DSCraft2D/DS_CRAFT/source/blocks/leaves.cpp
@@ -0,0 +1,15 @@
+#include <nds.h>
+#include "../ndsvideo.h"
+#include "block.h"
+#include "../blockID.h"
+u16* LEAVESgfx;
+void LEAVES_render(int x,int y){
+	if (y<192 && x<256 && x>(-32) && y>(-32)) createsprite32x32(x,y,LEAVESgfx,false,0);	
+}
+void LEAVES_setup(){
+	LEAVESgfx=oamAllocateGfx(&oamMain,SpriteSize_32x32,SpriteColorFormat_256Color);
+	char* blocktiles;
+	blocktiles=(char*)&blockTiles;
+	blocktiles+=(32*32)*LEAVES;
+	dmaCopy(blocktiles,LEAVESgfx,32*32);
+}
