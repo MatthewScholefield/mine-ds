9be077a88a1476b8f29535e904d17f92aef3bf9a
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-16 09:22:23 +0000

    MORE blocks (up to 20)

---
+++ DSCraft2D/DS_CRAFT/source/blocks/white_wood.h
@@ -0,0 +1,4 @@
+//A block has Two Functions
+void WHITE_WOOD_render(int a, int b); //Render the block
+void WHITE_WOOD_setup(); //And copy the blocks graphics
+
---
+++ DSCraft2D/DS_CRAFT/source/blocks/wood_dark.h
@@ -0,0 +1,4 @@
+//A block has Two Functions
+void DARK_WOOD_render(int a, int b); //Render the block
+void DARK_WOOD_setup(); //And copy the blocks graphics
+
