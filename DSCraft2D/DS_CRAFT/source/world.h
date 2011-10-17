a6e0eaedcd9db0d44c97880fddb7cc6e9b37010e
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-17 06:19:48 +0000

    Progress Did some TODO's

--- DSCraft2D/DS_CRAFT/source/world.h
+++ DSCraft2D/DS_CRAFT/source/world.h
@@ -1,7 +1,9 @@
 //Circular Dependances RUBBISH (google it(why isn't google a verb))
 #ifndef WORLD_H
+#define WORLD_WIDTH 31 //32 will be out of bonds  (Also more like chunk width and height)
+#define WORLD_HEIGHT 31 //32 wil be out of bounds (trial and error made me realise this (it is pointless))
 typedef struct{
-	int blocks[32][32]; //A world will have 32 by 32 blocks, for now as for testing
+	int blocks[WORLD_WIDTH+1][WORLD_HEIGHT+1]; //A world will have 32 by 32 blocks, for now as for testing
 	int ChoosedBlock;
 	bool DELmode;
 	int CamY;
