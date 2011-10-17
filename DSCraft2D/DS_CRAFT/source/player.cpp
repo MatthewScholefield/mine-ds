06fd8cc9b7ea8fe76e4b42b62170a1f75f7dd96a
CoolAs <goldenscruff@gmail.com>
2011-10-17 10:01:17 +0000

    Working Colision, (printed on top screen)

--- DSCraft2D/DS_CRAFT/source/player.cpp
+++ DSCraft2D/DS_CRAFT/source/player.cpp
@@ -1,9 +1,44 @@
 #include "ndsvideo.h" // Include the DS video stuff
+#include <stdio.h>
 #include <nds.h> //Include the DS library (for u16*)
 #include "player.h" //Include the player structure
 #include "PlayerR.h" //Include the player graphics
 #include "world.h" //A world!!!
+#include "gameshelper.h"
+#include "blockID.h"
+#define INSIDE 0
+#define LEFT 1
+#define UNDER 2
+#define RIGHT 3
+#define UP 4
+#define U_L 5
+#define U_R 6
 u16* playerGraphics;
+int colisionAdv(int blockx1,int blocky1,int blockx2,int blocky2,int x1,int y1,int x2,int y2){
+	if (spritecol(x1+8,y1,x2,y2,16,64,32,32)){
+		//A good square colision
+			if (blockx1==blockx2 && blocky1 == blocky2){
+				iprintf("Inside block\n");
+				return INSIDE;
+			}
+			if (blockx1==blockx2 && blocky1+1 == blocky2){
+				iprintf("Top of block\n");
+				return UNDER;
+			}
+			if (blockx1+1==blockx2 && blocky1 == blocky2){
+				iprintf("block on right\n");
+				return RIGHT;
+			}
+			if (blockx1-1==blockx2 && blocky1 == blocky2){
+				iprintf("block on left\n");
+				return LEFT;
+			}
+	}	
+	return 7;
+}
+void playerGravity(playerActor* player,worldObject* world){
+	
+}
 //Update the player
 //Called by main();
 void updateplayer(playerActor* player,worldObject* world){
@@ -33,6 +68,12 @@ void updateplayer(playerActor* player,worldObject* world){
 	else if (player->y<0) player->y=0;
 	//Draw the player on the screen
 	createsprite32x64(player->x-world->CamX,player->y-world->CamY,playerGraphics,keysHeld() & KEY_LEFT,1);
+	int i,j;
+	for (i=0;i<=WORLD_WIDTH;i++)
+		for(j=0;j<=WORLD_HEIGHT;j++)
+		{
+			if (world->blocks[i][j]!=AIR) colisionAdv(player->blockx,player->blocky,i,j,player->x,player->y,i*32,j*32);
+		}
 }
 //Stuff
 u16* playerGfx(){
