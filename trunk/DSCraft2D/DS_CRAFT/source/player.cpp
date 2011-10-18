1cf918b53e84cfc2876bf878bcc00d2b54d4a62d
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-18 09:41:08 +0000

    Colision

--- trunk/DSCraft2D/DS_CRAFT/source/player.cpp
+++ trunk/DSCraft2D/DS_CRAFT/source/player.cpp
@@ -15,28 +15,31 @@
 #define U_L 5
 #define U_R 6
 u16* playerGraphics;
+bool top;
 #define gravity 1
 int colisionAdv(int blockx1,int blocky1,int blockx2,int blocky2,int x1,int y1,int x2,int y2){
 	if (spritecol(x1+8,y1,x2,y2,16,64,32,32)){
 		//A good square colision
 			if (blockx1==blockx2 && blocky1 == blocky2){
-				iprintf("Inside block\n");
 				return INSIDE;
 			}
 			if (blockx1==blockx2 && blocky1+1 == blocky2){
-				iprintf("Top of block\n");
+				top=true;
 				return UNDER;
 			}
 			if (blockx1+1==blockx2 && blocky1 == blocky2){
-				iprintf("block on right\n");
+				return RIGHT;
+			}
+			if (blockx1+1==blockx2 && blocky1-1 == blocky2){
 				return RIGHT;
 			}
 			if (blockx1-1==blockx2 && blocky1 == blocky2){
-				iprintf("block on left\n");
+				return LEFT;
+			}
+			if (blockx1-1==blockx2 && blocky1-1 == blocky2){
 				return LEFT;
 			}
 			if (blockx1==blockx2 && blocky1-1 == blocky2){
-				iprintf("block above\n");
 				return UP;
 			}
 	}	
@@ -44,47 +47,47 @@ int colisionAdv(int blockx1,int blocky1,int blockx2,int blocky2,int x1,int y1,in
 }
 void playerGravity(playerActor* player,worldObject* world){
 	//colisionAdv(player->blockx,player->blocky,i,j,player->x,player->y,i*32,j*32)
+	printf("Gravity\n");
+	int x,y;
+	player->onblock=false;
+	if (player->vy<12) player->vy+=gravity; //vy is speed
+	for (x=0;x<=WORLD_WIDTH;x++)
+		for(y=0;y<=WORLD_HEIGHT;y++){
+			int result=colisionAdv(player->blockx,player->blocky,x,y,player->x,player->y,x*32,y*32);
+			if (world->blocks[x][y]==GRASS) GRASS_colision(player,world,x,y,result);
+			else if (world->blocks[x][y]==DIRT) DIRT_colision(player,world,x,y,result);
+			else if (world->blocks[x][y]==BEDROCK) BEDROCK_colision(player,world,x,y,result);
+		}
+	if(player->onblock==false) player->y+=player->vy;
+	if(keysHeld() & KEY_A && player->onblock==1)
+	{
+		player->vy=-11; // The -value is the rate which the guy jumps (DONT make it 1 hundred :P OR 1)
+		player->y-=1;
+	}
 }
-//Update the player
-//Called by main();
 void updateplayer(playerActor* player,worldObject* world){
 	//Scan the keys and move that minecraft guy, soon this will need the world values	
 	scanKeys();
-	//Move the player
-	if (keysHeld() & KEY_UP) player->y--;
-	else if (keysHeld() & KEY_DOWN) player->y++;
 	if (keysHeld() & KEY_LEFT)player->x--;
 	else if (keysHeld() & KEY_RIGHT) player->x++;
-	//Testing STUFF in the world.
-	if (keysHeld() & KEY_UP) world->CamY--;
-	else if (keysHeld() & KEY_DOWN ) world->CamY++;
-	if (keysHeld() & KEY_LEFT) world->CamX--;
-	else if (keysHeld() & KEY_RIGHT ) world->CamX++;
+	world->CamX=player->x-(256/2-16);
+	world->CamY=player->y-(192/2-32);
 	//Create the block positions
 	player->blockx=(player->x+15)/32;
 	player->blocky=(player->y+32)/32;	
 	//Stop at end of map
 	if (world->CamX>world_widthpx-(256-32)) world->CamX=world_widthpx-(256-32);
 	else if (world->CamX<0) world->CamX=0;
-	if (world->CamY>world_heightpx-(192-32)) world->CamY=world_heightpx-(192-32); //Why the take 32? try it without to find out, (it hides the last block)
+	if (world->CamY>world_heightpx-(192-32)) world->CamY=world_heightpx-(192-32);
 	else if (world->CamY<0) world->CamY=0;
 	if (player->x>world_widthpx) player->x=world_widthpx;
 	else if (player->x<0)player->x=0;
-	if (player->y>world_heightpx-32) player->y=world_heightpx-32;//Take one block (even though the last block is bedrock :P)
+	if (player->y>world_heightpx-32) player->y=world_heightpx-32;
 	else if (player->y<0) player->y=0;
-	//Draw the player on the screen
-	createsprite32x64(player->x-world->CamX,player->y-world->CamY,playerGraphics,keysHeld() & KEY_LEFT,1);
-	int i,j;
-	/*for (i=0;i<=WORLD_WIDTH;i++)
-		for(j=0;j<=WORLD_HEIGHT;j++)
-		{
-			if (world->blocks[i][j]!=AIR) colisionAdv(player->blockx,player->blocky,i,j,player->x,player->y,i*32,j*32);
-		}*/
 	playerGravity(player,world);
+	createsprite32x64(player->x-world->CamX,player->y-world->CamY,playerGraphics,keysHeld() & KEY_LEFT,1);
 }
-//Stuff
 u16* playerGfx(){
-	//return the player Graphics
 	return playerGraphics;
 }
 void playerCreateGfx(){
