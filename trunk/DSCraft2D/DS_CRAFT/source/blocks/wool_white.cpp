880e19f549351812ac7048363ae4a4f41df6dd70
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-23 02:06:35 +0000

    Wool Hurts You!

--- trunk/DSCraft2D/DS_CRAFT/source/blocks/wool_white.cpp
+++ trunk/DSCraft2D/DS_CRAFT/source/blocks/wool_white.cpp
@@ -7,14 +7,21 @@
 int hurttime;
 u16* WOOL_WHITEgfx;
 void WOOL_WHITE_render(int x,int y){
-	createsprite32x32(x,y,WOOL_WHITEgfx,false,0);	
+createsprite32x32(x,y,WOOL_WHITEgfx,false,0);
 }
 void WOOL_WHITE_setup(){
-	WOOL_WHITEgfx=oamAllocateGfx(&oamMain,SpriteSize_32x32,SpriteColorFormat_256Color);
-	char* blocktiles;
-	blocktiles=(char*)&blockTiles;
-	blocktiles+=(32*32)*WOOL_WHITE;
-	dmaCopy(blocktiles,WOOL_WHITEgfx,32*32);
+WOOL_WHITEgfx=oamAllocateGfx(&oamMain,SpriteSize_32x32,SpriteColorFormat_256Color);
+char* blocktiles;
+blocktiles=(char*)&blockTiles;
+blocktiles+=(32*32)*WOOL_WHITE;
+dmaCopy(blocktiles,WOOL_WHITEgfx,32*32);
+}
+void hurt(playerActor* player){
+hurttime++; //Add one to the framecount
+if (hurttime==60){//Every 1 second loose a heart
+player->health--; //take one heart.
+	hurttime=0;
+}
 }
 void WOOL_WHITE_colision(playerActor* player,worldObject* world,int bx,int by,int result){
     hurttime++; //Add one to the framecount
@@ -38,3 +45,21 @@ void WOOL_WHITE_colision(playerActor* player,worldObject* world,int bx,int by,in
 		player->y+=2;	
 	}
 }
+if (result==3){//player colides on right
+player->x-=2; //Move him back one so he is not coliding anymore :)
+
+hurt(player);
+}
+if (result==1){//colides on left
+player->x+=2;
+
+hurt(player);
+}
+if (result==4){
+//colision upwards
+player->vy=0;
+player->y+=2;
+
+hurt(player);
+}
+} 
--- trunk/DSCraft2D/DS_CRAFT/source/blocks/wool_white.h
+++ trunk/DSCraft2D/DS_CRAFT/source/blocks/wool_white.h
@@ -2,5 +2,4 @@
 #include "../world.h"
 void WOOL_WHITE_render(int a, int b);
 void WOOL_WHITE_setup();
-void WOOL_WHITE_update(int bx,int by,worldObject* world,playerActor* player);
 void WOOL_WHITE_colision(playerActor* player,worldObject* world,int bx,int by,int result);
