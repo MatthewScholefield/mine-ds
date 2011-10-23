2425a0f4a571e81dc6759a7ab830726930d721c6
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-23 10:39:12 +0000

    Saving for world1

--- DSCraft2D/DS_CRAFT/source/saver.cpp
+++ DSCraft2D/DS_CRAFT/source/saver.cpp
@@ -1,18 +1,33 @@
 #include <nds.h>
 #include <fat.h>
+#include <stdio.h>
 #include "player.h"
 #include "world.h"
 bool fatinited;
 touchPosition touch2;
-void saveMenu(){
+void saveMenu(worldObject* world){
 	lcdSwap();
+	worldObject STUF;
 	bool fertig=false;
 	while(!fertig){
 		scanKeys();
 		if (keysDown() & KEY_TOUCH){	
 			touchRead(&touch2);
 			if (touch2.px>=138 && touch2.px<=180 && touch2.py>=27 && touch2.py<=46){
-				//Load World1			
+				//Load World1	
+				if (fatinited){
+					FILE* save_file = fopen("world_1_w.sav", "rb");		
+					fread(world, 1, sizeof(STUF), save_file);
+					fclose(save_file);
+				}		
+			}
+			if (touch2.px>=187 && touch2.px<=227 && touch2.py>=27 && touch2.py<=46){
+				//Load World1	
+				if (fatinited){
+					FILE* save_file = fopen("world_1_w.sav", "wb");		
+					fwrite(world, 1, sizeof(STUF), save_file);
+					fclose(save_file);
+				}		
 			}
 			fertig=true;
 		}
@@ -21,7 +36,7 @@ void saveMenu(){
 }
 void saveUpdate(worldObject* world,playerActor* player){
 	if (keysHeld() & KEY_START){
-		saveMenu();
+		saveMenu(world);
 	}
 }
 void saveInit(){
