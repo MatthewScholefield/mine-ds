422b257bf544de95619aa41aabd086b528a9d90d
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-10-23 10:20:44 +0000

    Less than or equal :P (bug fix)

--- trunk/DSCraft2D/DS_CRAFT/source/saver.cpp
+++ trunk/DSCraft2D/DS_CRAFT/source/saver.cpp
@@ -1,7 +1,29 @@
+#include <nds.h>
+#include <fat.h>
 #include "player.h"
 #include "world.h"
+bool fatinited;
+touchPosition touch2;
+void saveMenu(){
+	lcdSwap();
+	bool fertig=false;
+	while(!fertig){
+		scanKeys();
+		if (keysDown() & KEY_TOUCH){	
+			touchRead(&touch2);
+			if (touch2.px>=138 && touch2.px<=180 && touch2.py>=27 && touch2.py<=46){
+				//Load World1			
+			}
+			fertig=true;
+		}
+	}
+	lcdMainOnBottom();
+}
 void saveUpdate(worldObject* world,playerActor* player){
-	
+	if (keysHeld() & KEY_START){
+		saveMenu();
+	}
 }
 void saveInit(){
+	fatinited=fatInitDefault();
 }
