5a8bd400685ea04c1806e4833e8ab7ab0de9a325
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-11-02 06:39:29 +0000

    Hearts now show

--- DSCraft2D/DS_CRAFT/source/top-screen.cpp
+++ DSCraft2D/DS_CRAFT/source/top-screen.cpp
@@ -12,212 +12,181 @@ int oldblock=0;
 u16* gfx;
 u16* heartgfx;
 void subBGSetup(){ //Its a setup function, not a update function :P
-	oamInit(&oamSub, SpriteMapping_1D_32, true);
-	vramSetBankD(VRAM_D_SUB_SPRITE);
-	vramSetBankI(VRAM_I_LCD);
-	gfx = oamAllocateGfx(&oamSub, SpriteSize_32x32, SpriteColorFormat_256Color);
-	heartgfx = oamAllocateGfx(&oamSub, SpriteSize_8x8, SpriteColorFormat_256Color);
-   	int bg = bgInitSub(3, BgType_Bmp16, BgSize_B16_256x256, 0,0);
-    	dmaCopy(subscreen2Bitmap, bgGetGfxPtr(bg), subscreen2BitmapLen);
-	dmaCopy(blockPal,VRAM_I_EXT_SPR_PALETTE[0],blockPalLen);
-	dmaCopy(h1Pal,VRAM_I_EXT_SPR_PALETTE[1],h1PalLen);
-	char* h1gfx;//Copy the graphics into memory
-  	h1gfx=(char*)h1Tiles;
-	dmaCopy(h1gfx,heartgfx,8*8);
-	vramSetBankI(VRAM_I_SUB_SPRITE_EXT_PALETTE);
+        oamInit(&oamSub, SpriteMapping_1D_32, true);
+        vramSetBankD(VRAM_D_SUB_SPRITE);
+        vramSetBankI(VRAM_I_LCD);
+        gfx = oamAllocateGfx(&oamSub, SpriteSize_32x32, SpriteColorFormat_256Color);
+        heartgfx = oamAllocateGfx(&oamSub, SpriteSize_8x8, SpriteColorFormat_256Color);
+        int bg = bgInitSub(3, BgType_Bmp16, BgSize_B16_256x256, 0,0);
+        dmaCopy(subscreen2Bitmap, bgGetGfxPtr(bg), subscreen2BitmapLen);
+        dmaCopy(blockPal,VRAM_I_EXT_SPR_PALETTE[0],blockPalLen);
+        dmaCopy(h1Pal,VRAM_I_EXT_SPR_PALETTE[1],h1PalLen);
+        char* h1gfx;//Copy the graphics into memory
+        h1gfx=(char*)h1Tiles;
+        dmaCopy(h1gfx,heartgfx,8*8);
+        vramSetBankI(VRAM_I_SUB_SPRITE_EXT_PALETTE);
 }
 void subLifes(int count){
-    oamClear(&oamSub,2,11); //clear the 10 lives sprites
+    oamClear(&oamSub,3,11); //clear the 10 lives sprites
     int i=1;
-	if (count>=1){
-		oamSet(&oamSub,3, //Then draw the sprite on the screen
-		1, 
-		176, 
-		0, 
-		1,
-		SpriteSize_8x8, 
-		SpriteColorFormat_256Color, 
-		heartgfx, 
-		-1, 
-		false, 
-		false,			
-		false, false, 
-		false	
-		);   
-	if (count>=2){
-		oamSet(&oamSub,4, //Then draw the sprite on the screen
-		9, 
-		176, 
-		0, 
-		1,
-		SpriteSize_8x8, 
-		SpriteColorFormat_256Color, 
-		heartgfx, 
-		-1, 
-		false, 
-		false,			
-		false, false, 
-		false	
-		); 
-	if (count>=3){
-		oamSet(&oamSub,5, //Then draw the sprite on the screen
-		18, 
-		176, 
-		0, 
-		1,
-		SpriteSize_8x8, 
-		SpriteColorFormat_256Color, 
-		heartgfx, 
-		-1, 
-		false, 
-		false,			
-		false, false, 
-		false	
-		);  
-	if (count>=4){
-		oamSet(&oamSub,6, //Then draw the sprite on the screen
-		27, 
-		176, 
-		0, 
-		1,
-		SpriteSize_8x8, 
-		SpriteColorFormat_256Color, 
-		heartgfx, 
-		-1, 
-		false, 
-		false,			
-		false, false, 
-		false	
-		); 
-	if (count>=5){
-	oamSet(&oamSub,7, //Then draw the sprite on the screen
-	36, 
-	176, 
-	0, 
-	1,
-	SpriteSize_8x8, 
-	SpriteColorFormat_256Color, 
-	heartgfx, 
-	-1, 
-	false, 
-	false,			
-	false, false, 
-	false	
-	);  
-	if (count>=6){
-	oamSet(&oamSub,8, //Then draw the sprite on the screen
-	45, 
-	176, 
-	0, 
-	1,
-	SpriteSize_8x8, 
-	SpriteColorFormat_256Color, 
-	heartgfx, 
-	-1, 
-	false, 
-	false,			
-	false, false, 
-	false	
-	);  
-	if (count>=7){
-	oamSet(&oamSub,9, //Then draw the sprite on the screen
-	54, 
-	176, 
-	0, 
-	1,
-	SpriteSize_8x8, 
-	SpriteColorFormat_256Color, 
-	heartgfx, 
-	-1, 
-	false, 
-	false,			
-	false, false, 
-	false	
-	);
-	if (count>=8){
-	oamSet(&oamSub,10, //Then draw the sprite on the screen
-	63, 
-	176, 
-	0, 
-	1,
-	SpriteSize_8x8, 
-	SpriteColorFormat_256Color, 
-	heartgfx, 
-	-1, 
-	false, 
-	false,			
-	false, false, 
-	false	
-	); 
-	if (count>=9){
-	oamSet(&oamSub,11, //Then draw the sprite on the screen
-	72, 
-	176, 
-	0, 
-	1,
-	SpriteSize_8x8, 
-	SpriteColorFormat_256Color, 
-	heartgfx, 
-	-1, 
-	false, 
-	false,			
-	false, false, 
-	false	
-	);  
-	if (count>=10){
-	oamSet(&oamSub,12, //Then draw the sprite on the screen
-	81, 
-	176, 
-	0, 
-	1,
-	SpriteSize_8x8, 
-	SpriteColorFormat_256Color, 
-	heartgfx, 
-	-1, 
-	false, 
-	false,			
-	false, false, 
-	false	
-	);  
-	}  
-	}  
-	}   
-	} 
-	} 
-	} 
-	}
-	} 
-	}
-	}
+        if (count>=1){
+
+        oamSet(&oamSub,3, //Then draw the sprite on the screen
+        1, 
+        176, 
+        0, 
+        1,
+        SpriteSize_8x8, 
+        SpriteColorFormat_256Color, 
+        heartgfx, 
+        -1, 
+        false, 
+        false,                  
+        false, false, 
+        false   
+        );   
+        if (count>=2){
+        oamSet(&oamSub,4, //Then draw the sprite on the screen
+        9, 
+        176, 
+        0, 
+        1,
+        SpriteSize_8x8, 
+        SpriteColorFormat_256Color, 
+        heartgfx, 
+        -1, 
+        false, 
+        false,                  
+        false, false, 
+        false   
+        ); 
+        if (count>=3){
+        oamSet(&oamSub,5, //Then draw the sprite on the screen
+        18, 
+        176, 
+        0, 
+        1,
+        SpriteSize_8x8, 
+        SpriteColorFormat_256Color, 
+        heartgfx, 
+        -1, 
+        false, 
+        false,                  
+        false, false, 
+        false   
+        );  
+        if (count>=4){
+        oamSet(&oamSub,6, //Then draw the sprite on the screen
+        27, 
+        176, 
+        0, 
+        1,
+        SpriteSize_8x8, 
+        SpriteColorFormat_256Color, 
+        heartgfx, 
+        -1, 
+        false, 
+        false,                  
+        false, false, 
+        false   
+        ); 
+        if (count>=5){
+        oamSet(&oamSub,7, //Then draw the sprite on the screen
+        36, 
+        176, 
+        0, 
+        1,
+        SpriteSize_8x8, 
+        SpriteColorFormat_256Color, 
+        heartgfx, 
+        -1, 
+        false, 
+        false,                  
+        false, false, 
+        false   
+        );  
+        if (count>=6){
+        oamSet(&oamSub,8, //Then draw the sprite on the screen
+        45, 
+        176, 
+        0, 
+        1,
+        SpriteSize_8x8, 
+        SpriteColorFormat_256Color, 
+        heartgfx, 
+        -1, 
+        false, 
+        false,                  
+        false, false, 
+        false   
+        );  
+        if (count>=7){
+        oamSet(&oamSub,9, //Then draw the sprite on the screen
+        54, 
+        176, 
+        0, 
+        1,
+        SpriteSize_8x8, 
+        SpriteColorFormat_256Color, 
+        heartgfx, 
+        -1, 
+        false, 
+        false,                  
+        false, false, 
+        false   
+        );
+        if (count>=8){
+        oamSet(&oamSub,10, //Then draw the sprite on the screen
+        63, 
+        176, 
+        0, 
+        1,
+        SpriteSize_8x8, 
+        SpriteColorFormat_256Color, 
+        heartgfx, 
+        -1, 
+        false, 
+        false,                  
+        false, false, 
+        false   
+        );  
+        }   
+        } 
+        } 
+        } 
+        }
+        } 
+        }
+        }
+
 }
 
 void subShowBlock(int block){
-	if (block==AIR) oamClear(&oamSub,0,1); //If the block is air, remove all of the sprite's with oam Clear
-	if (block==PLACED_LOG) block=LOG; //If the block is a PLACED_LOG pretend it is a normal LOG
-	if (block==PLACED_LOG_W) block=WHITE_WOOD; //Look up
-	if (block==PLACED_LOG_D) block=DARK_WOOD;//^
-	if (block==PLACED_LEAF) block=LEAVES;//^
-	if (block<128 && block!=oldblock){//If the block is actually in the block.png file
-		char* blockgfx;//Copy the graphics into memory
-		blockgfx=(char*)blockTiles;
-		blockgfx+=(32*32)*block;//this is in the block/*.cpp
-		dmaCopy(blockgfx,gfx,32*32);//^
-		oamSet(&oamSub,0, //Then draw the sprite on the screen
-			38, 
-			32, 
-			0, 
-			0,
-			SpriteSize_32x32, 
-			SpriteColorFormat_256Color, 
-			gfx, 
-			-1, 
-			false, 
-			false,			
-			false, false, 
-			false	
-			);    	
-	}
-	oldblock=block;
+        if (block==AIR) oamClear(&oamSub,0,3); //If the block is air, remove all of the sprite's with oam Clear
+        if (block==PLACED_LOG) block=LOG; //If the block is a PLACED_LOG pretend it is a normal LOG
+        if (block==PLACED_LOG_W) block=WHITE_WOOD; //Look up
+        if (block==PLACED_LOG_D) block=DARK_WOOD;//^
+        if (block==PLACED_LEAF) block=LEAVES;//^
+        if (block<128){//If the block is actually in the block.png file
+                char* blockgfx;//Copy the graphics into memory
+                blockgfx=(char*)blockTiles;
+                blockgfx+=(32*32)*block;//this is in the block/*.cpp
+                dmaCopy(blockgfx,gfx,32*32);//^
+                oamSet(&oamSub,0, //Then draw the sprite on the screen
+                        38, 
+                        32, 
+                        0, 
+                        0,
+                        SpriteSize_32x32, 
+                        SpriteColorFormat_256Color, 
+                        gfx, 
+                        -1, 
+                        false, 
+                        false,                  
+                        false, false, 
+                        false   
+                        );      
+        }
 
 }
-	
+        
