3c0f0f5b20c14ed04e7a12aa576843b299eab25e
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-11-02 05:46:55 +0000

    Thanks To Dirbaio we can spawn 100 (visable) Mobs without lagg

--- trunk/DSCraft2D/DS_CRAFT/source/blockupdate.cpp
+++ trunk/DSCraft2D/DS_CRAFT/source/blockupdate.cpp
@@ -5,13 +5,16 @@
 #include "blockID.h"
 void updateBlocks(worldObject* world,playerActor* player){
 	int i,j;
-	for (i=player->blockx-16;i<=player->blockx+16 && i<=WORLD_WIDTH;i++)//Cycle through the block array
-		for (j=player->blocky-16;j<=player->blocky+16 && j<=WORLD_HEIGHT;j++){
+	for (i=0;i<=WORLD_WIDTH;i++)//Cycle through the block array
+		for (j=0;j<=WORLD_HEIGHT;j++){
 			if (i<=-1) i=-1;
 			if (j<=-1) j=-1;
-			if (world->blocks[i][j]==TORCH) TORCH_update(i,j,world,player);
-			else if (world->blocks[i][j]==GRASS) GRASS_update(i,j,world,player);
-			else if (world->blocks[i][j]==DIRT) DIRT_update(i,j,world,player);
+			switch(world->blocks[i][j])
+			{
+				case TORCH: TORCH_update(i,j,world,player); break;
+				case GRASS: GRASS_update(i,j,world,player); break;
+				case DIRT: DIRT_update(i,j,world,player); break;
+			}
 		}
 }
 			
--- trunk/DSCraft2D/DS_CRAFT/source/world.h
+++ trunk/DSCraft2D/DS_CRAFT/source/world.h
@@ -1,10 +1,10 @@
 //Circular Dependances RUBBISH (google it(why isn't google a verb))
 #ifndef WORLD_H
-#define WORLD_HEIGHT 48
+#define WORLD_HEIGHT 64
 #define world_heightpx WORLD_HEIGHT*32
 #define world_widthpx WORLD_WIDTH*32
 #define WORLD_WIDTHpx WORLD_WIDTH*32
-#define WORLD_WIDTH 48
+#define WORLD_WIDTH 256
 typedef struct{
 	int blocks[WORLD_WIDTH+16][WORLD_HEIGHT+16]; //A world will have 32 by 32 blocks, for now as for testing
 	int data[WORLD_WIDTH+16][WORLD_HEIGHT+16]; //Plus16??? So If something happens the DS does not read outside of the array
