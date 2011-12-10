c41a168446eb0c42ab9a8c931abb9b73f63e1c6e
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2011-12-10 09:50:24 +0000

    Added an inventory Which is VERY BUGGY... Reverted day-night to r140-r149...

---
+++ trunk/DSCraft2D/DS_CRAFT/source/inventory.cpp
@@ -0,0 +1,90 @@
+#include "inventory.h"
+#include "blockID.h"
+#include "allblocks.h"
+#include <nds.h>
+#include <stdio.h>
+#include "world.h"
+#include "saver.h" //Saver Loades the numbers into VRAM...
+char buffer[4];	
+u16* x;
+inventoryStruct inventory;
+void inventoryInit(){
+	int i;
+	for (i=0;i<=35;i++){
+		inventory.blockID[i]=255;
+		inventory.blockAmount[i]=0;
+	}
+	x=numberReturn(10); //10 is the "x" Symbol... (Yeah I know suckish implementation...)
+}
+int inventoryAdd(int blockID){
+	int i;
+	int found=0;
+	int foundID=0;
+	for (i=0;i<=35;i++){
+	//Find an entry of "blockID"
+		if (inventory.blockID[i]==blockID){
+			foundID=i;//Keep the entry """address""" in the variable foundID...
+			inventory.blockAmount[foundID]++; //Add one to the amount of those blocks...
+			i=37;
+			return 1;
+		}
+		else if (inventory.blockID[i]==255){
+			//We have found a AIR block which can serve as an empty space...
+			foundID=i;
+			inventory.blockID[foundID]=blockID;
+			inventory.blockAmount[foundID]=0;
+			i=37;	
+			return 1;
+		}
+	}
+	return 0;
+}
+int inventoryRemove(int blockID){
+	int i;
+	int found=0;
+	int foundID=0;
+	for (i=0;i<=35;i++){
+	//Find an entry of "blockID"
+		if (inventory.blockID[i]==blockID){
+			found=1;
+			foundID=i;//Keep the entry """address""" in the variable foundID...
+			i=37;
+		}
+	}
+	if (found==0) return 0;
+	else if (found==1){
+		if (inventory.blockAmount[foundID]>0) inventory.blockAmount[foundID]--;
+		else{
+			inventory.blockID[foundID]=255;
+			inventory.blockAmount[foundID]=0;
+		}
+	}
+	return 1;
+}
+void DrawAmount(worldObject* world){
+	int i;
+	int foundID;
+	bool found=false;
+	int blockID=world->ChoosedBlock;
+	for (i=0;i<=35;i++){
+	//Find an entry of "blockID"
+		if (inventory.blockID[i]==blockID){
+			foundID=i;//Keep the entry """address""" in the variable foundID...
+			i=37;
+			found=true;
+		}
+	}
+	if (!found){
+		world->ChoosedBlock=AIR;		
+	}
+	else if (found){
+		sprintf (buffer, "%daa",inventory.blockAmount[foundID]);
+		int number1=buffer[0]-48;
+		int number2=buffer[1]-48;
+		if (number1!=0)oamSet(&oamSub,20,70,64,0,3,SpriteSize_16x16,SpriteColorFormat_256Color,numberReturn(10),-1,false,false,false, false,false); 
+		if (number1!=0)oamSet(&oamSub,21,86,64,0,3,SpriteSize_16x16,SpriteColorFormat_256Color,numberReturn(number1),-1,false,false,false, false,false); 
+		oamSet(&oamSub,22,102,64,0,3,SpriteSize_16x16,SpriteColorFormat_256Color,numberReturn(number2),-1,false,false,false, false,false); 
+	}
+	
+}
+
