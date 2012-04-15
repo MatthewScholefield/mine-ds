340c986180777691ea8ea53c9791e984de058898
CoolAs <goldenscruff@gmail.com>
2012-04-15 11:02:12 +0000

    ASDF

--- MineDS/source/mining.cpp
+++ MineDS/source/mining.cpp
@@ -14,7 +14,7 @@ void miningUpdate(worldObject* world,int a,int b,touchPosition touch,int keys)
 		{
 			world->blocks[x][y]=TORCH;
 			world->lightemit[x][y]=1;
-			brightnessSpread(world,x,y,2);
+			brightnessSpread(world,x,y,0);
 		}
 		else
 		{
