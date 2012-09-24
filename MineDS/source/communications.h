76746c25f48f736cd5564fca07b54344e5c4efe1
CoolAs <goldenscruff@gmail.com>
2012-09-24 10:52:45 +0000

    Slow Speeds? Use Run Length Encoding!!!!! n00b explaination: Download now is MUCH quicker!

--- MineDS/source/communications.h
+++ MineDS/source/communications.h
@@ -4,5 +4,5 @@ void connectSuccess();
 void communicationInit(worldObject* world2);
 void recieveWorld(worldObject* world2);
 void connectCode(int code2);
-void setBlock(int x,int y,int block,int bgblock);
+void setBlock(int x,int y,int block,int bgblock,int amount);
 void sendblock(int a,int x, int y);
