73b523d91439b2f89d5a4595c7f4f54c7996b7c1
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2012-09-25 06:30:59 +0000

    Commited missing Files!

---
+++ trunk/MineDS/source/mobs/mobMPlayer.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "../world.h"
+#include "../blocks.h"
+#include "../graphics/graphics.h"
+#include "../debugflag.h"
+#include "mobMPlayer.h"
+#include "../blockID.h"
+#include <nds.h>
+//ASDF?
+Graphic MplayerMobGraphic[3];
+MplayerMob::MplayerMob()
+{
+	x=0;
+	y=0;
+	vy=0;
+	vx=0;
+	alive=false;
+	onground=false;
+	animation=0;
+	mobtype=2;
+}
+MplayerMob::MplayerMob(int a,int b)
+{
+	gravity=4;
+	gravityValue=4;
+	sx=6;
+	sy=32;
+	x=a;
+	y=b;
+	vy=0;
+	vx=0;
+	alive=false;
+	onground=false;
+	animation=0;
+	mobtype=2;
+}
+void MplayerMob::updateMob(worldObject* world)
+{
+	if (x-world->CamX>-16 && x-world->CamX<256+16 && y-world->CamY>-32 && y-world->CamY<256)
+	showGraphic(&MplayerMobGraphic[0],x-world->CamX - (animation ? 10:0),y-world->CamY,animation ? true:false);
+}
+void MplayerMob::sendWifiUpdate()
+{
+	
+}
+void MplayerMob::saveToFile(FILE* pFile)
+{
+}
+void MplayerMob::loadFromFile(FILE* pFile)
+{
+}
+bool canMplayerMobSpawnHere(worldObject* world,int x,int y)
+{
+	y++;
+	if (!isBlockWalkThrough(world->blocks[x][y+1]) && isBlockWalkThrough(world->blocks[x][y]) && world->blocks[x][y]!=CACTUS) return true;
+	return false;
+}
+void MplayerMobInit()
+{
+	loadGraphic(&MplayerMobGraphic[0],true,0);
+}
+
---
+++ trunk/MineDS/source/mobs/mobMPlayer.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "../world.h"
+#include "baseMob.h"
+bool canMplayerMobSpawnHere(worldObject* world,int x,int y);
+
+class MplayerMob : public baseMob
+{
+    public:
+	/*int x;
+	int y;
+	int vy;
+	int vx;
+	bool alive;
+	bool host; // Was this mob spawn'd by this nds?*/
+    //bool potioneffects[5];
+	virtual void saveToFile(FILE* sFile);
+	virtual void loadFromFile(FILE* sFile);
+	virtual void sendWifiUpdate();
+    	virtual void updateMob(worldObject* world);	
+	MplayerMob();
+   	MplayerMob(int x,int y);
+};
+void MplayerMobInit();
