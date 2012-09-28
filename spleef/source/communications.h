0b5281e673c002e8a2c527f752c62013c137ade0
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2012-09-28 10:24:41 +0000

    BUGFIX

--- spleef/source/communications.h
+++ spleef/source/communications.h
@@ -3,4 +3,5 @@ int doHandshake();
 void connectSuccess();
 void connectCode(int code2);
 void setBlock(int x,int y);
+void sendReset();
 
--- spleef/source/mainGame.h
+++ spleef/source/mainGame.h
@@ -3,3 +3,4 @@ void mainGame();
 void recievedMessage();
 void recievePositionsFromDS(int x,int y,int facing,int frame);
 void breakBlock(int x,int y);
+void resetGame();
