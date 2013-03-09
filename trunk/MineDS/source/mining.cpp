0752ff934d7b3b1439d2bb9ee6188affdc317b9d
matthew3311999@gmail.com <matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2013-03-09 04:43:14 +0000

    Added block updates! (Cacti, flowers, torches, and snow tops) But they're very horribly coded; You might want to recode it more efficiently.

--- trunk/MineDS/source/mining.cpp
+++ trunk/MineDS/source/mining.cpp
@@ -39,22 +39,64 @@ void blocksCanPlace()
 void setBlock(worldObject* world, int x,int y)
 {
 	bool something=true;
-	if (keysHeld() & KEY_DOWN)
+        int c = 1;
+	if ((keysHeld() & KEY_DOWN) || selectedblock==CACTUS)
 	{
 		if (world->bgblocks[x][y]!=AIR) something=false;
 	}
 	if (world->blocks[x][y]==AIR && something)
 	{
-		if (keysHeld() & KEY_DOWN) world->bgblocks[x][y]=selectedblock;
-		else world->blocks[x][y]=selectedblock;
+		if ((keysHeld() & KEY_DOWN) || (selectedblock==CACTUS && (world->bgblocks[x][y+1]==SAND || world->bgblocks[x][y+1]==CACTUS)))
+                {
+                    world->bgblocks[x][y]=selectedblock;
+                    if (selectedblock==SNOW_TOP && (world->bgblocks[x][y+1]==GRASS || world->bgblocks[x][y+1]==DIRT))
+                {
+                        world->bgblocks[x][y+1]=SNOW_GRASS;
+                }
+                }
+                else if ((selectedblock==RED_FLOWER || selectedblock==YELLOW_FLOWER || selectedblock==TORCH) && world->bgblocks[x][y+1]!=AIR && world->bgblocks[x][y+1]!=RED_FLOWER && world->bgblocks[x][y+1]!=YELLOW_FLOWER && world->bgblocks[x][y+1]!=TORCH && world->bgblocks[x][y+1]!=SNOW_TOP)
+                    world->blocks[x][y]=selectedblock;
+                else if (selectedblock==SNOW_TOP && (world->blocks[x][y+1]==GRASS || world->blocks[x][y+1]==DIRT))
+                {
+                        world->blocks[x][y]=selectedblock;
+                        world->blocks[x][y+1]=SNOW_GRASS;
+                }
+		else if (selectedblock!=CACTUS && selectedblock!=SNOW_TOP && selectedblock!=RED_FLOWER && selectedblock!=YELLOW_FLOWER && selectedblock!=TORCH)
+                {
+                    world->blocks[x][y]=selectedblock;
+                }
 	}
 	else if (world->blocks[x][y]!=AIR)
 	{
-	 world->blocks[x][y]=AIR;
+                if (world->blocks[x][y]==SNOW_TOP)
+                {
+                        world->blocks[x][y+1]=GRASS;
+                        if (world->bgblocks[x][y]==SNOW_TOP)
+                            world->bgblocks[x][y]=AIR;
+                }
+                world->blocks[x][y]=AIR;
+                if ((world->blocks[x][y-1]==SNOW_TOP) || (world->bgblocks[x][y-1]==SNOW_TOP))
+                {
+                        world->blocks[x][y-1]=AIR;
+                        world->bgblocks[x][y-1]=AIR;
+                }
 	}
 	else if (world->blocks[x][y]==AIR)
 	{
-	 world->bgblocks[x][y]=AIR;
+                if (world->bgblocks[x][y]==SNOW_TOP)
+                    world->bgblocks[x][y+1]=GRASS;
+                world->bgblocks[x][y]=AIR;
+                while ((world->blocks[x][y-c]==CACTUS) || (world->bgblocks[x][y-c]==CACTUS))
+                {
+                        world->blocks[x][y-c]=AIR;
+                        world->bgblocks[x][y-c]=AIR;
+                        c++;
+                }
+                if ((world->blocks[x][y-1]==SNOW_TOP) || (world->bgblocks[x][y-1]==SNOW_TOP))
+                {
+                        world->blocks[x][y-1]=AIR;
+                        world->bgblocks[x][y-1]=AIR;
+                }
 	}
 	else return;
 	//Send a WIFI Update now, if wifi is enabled!
