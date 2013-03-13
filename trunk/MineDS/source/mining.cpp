b256eef4ecfab84c2a2f65cb448b70ea09fc6cff
matthew3311999@gmail.com <matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2013-03-13 22:04:49 +0000

    Survival WIP. Only Limitted blocks so far.

--- trunk/MineDS/source/mining.cpp
+++ trunk/MineDS/source/mining.cpp
@@ -12,11 +12,13 @@
 #include "nifi.h"
 #include "blockupdate.h"
 #include "titlescreen.h"
-#define NUM_BLOCKS 47
+int NUM_BLOCKS;
+int BL = 0;
 int selectedblock=0;
 bool loadedgraphic=false;
 Graphic topBlock;
 extern bool LRC;
+extern int GM;
 bool incutscene = false;
 bool canPlaceBlocks=true;
 int framecounting=0;
@@ -47,25 +49,29 @@ void setBlock(worldObject* world, int x,int y)
 	}
 	if (world->blocks[x][y]==AIR && something)
 	{
-		if ((keysHeld() & KEY_DOWN))
+		if (((keysHeld() & KEY_DOWN) && BL > 0) || ((keysHeld() & KEY_DOWN) && GM == 1))
                 {
         		world->bgblocks[x][y]=selectedblock;
+                        BL--; //Subtracts one to Block Limit Variable
 			checkBlockPlace(x,y,world,true);		
                 }
-                else 
+                else if (BL > 0 || GM == 1)
 		{
 			world->blocks[x][y]=selectedblock;
+                        BL--; //Subtracts one to Block Limit Variable
 			checkBlockPlace(x,y,world,false);
 		}
 	}
 	else if (world->blocks[x][y]!=AIR)
 	{
                 world->blocks[x][y]=AIR;
+                BL++; //Adds one to Block Limit Variable
 		checkBlockDelete(x,y,world,false);
 	}
 	else if (world->blocks[x][y]==AIR)
 	{
                 world->bgblocks[x][y]=AIR;
+                BL++; //Adds one to Block Limit Variable
 		checkBlockDelete(x,y,world,true);
 	}
 	else return;
@@ -79,6 +85,12 @@ void setBlock(worldObject* world, int x,int y)
 }
 void miningUpdate(worldObject* world,int a,int b,touchPosition touch,int keys) // keys = keysDown();
 {
+        if (GM==1){
+        NUM_BLOCKS=47;
+        }
+        else {
+                NUM_BLOCKS=4;
+        }
 	if (keys & KEY_TOUCH)
 	{
 		int x = (touch.px+a)/16;
