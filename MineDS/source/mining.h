061114e153744bea7251736c20a49ff86cd9987e
matthew3311999@gmail.com <matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2013-04-05 22:19:31 +0000

    REAL inventories, creative, and survival (that integrate with with multiplayer by putting creative by default) and made checkBlockPlace a boolean

--- MineDS/source/mining.h
+++ MineDS/source/mining.h
@@ -1,5 +1,7 @@
 #include "world.h"
 #include <nds.h>
+#define NUM_BLOCKS 47
+int getSelectedblock();
 void miningUpdate(worldObject* world,int sx,int sy,touchPosition touch, int keys);
 void blocksCanPlace();
 void miningSetScene(bool a);
