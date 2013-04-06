49dfc338874a75f05886300dead4fc7a655b9b2a
matthew3311999@gmail.com <matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2013-04-06 02:26:26 +0000

    You can revert this if you would like. I attempted to add the inventory graphics to the game, yet they will not appear. Do yo know the problem? Note: I disabled the 'initInvGraphics()' because All it does is draw a grey background and I wasn't sure if it would Hide the inventory graphics if they actually did show up. And When I tried to replace all the number 20s to the define I made, there were syntax errors. Though I don't know why considering I though defines are compiled as the excact number they are set at in which what happened should not have happened.

--- MineDS/source/inventory.cpp
+++ MineDS/source/inventory.cpp
@@ -5,14 +5,17 @@
 -----------------------------------------
 */
 #include "mining.h" //for NUM_BLOCKS
+#include "graphics\inventoryGraphics.h"
 int inventory[NUM_BLOCKS];
 void addInventory(int blockID, int amount) //adds the specified amount to a blockvalue
 {
 	inventory [blockID] += amount;
+	updateInvGraphics();
 }
 void subInventory (int blockID, int amount) //subtracts the specified amount to a blockvalue
 {
 	inventory [blockID] -= amount;
+	updateInvGraphics();
 }
 int checkInventory (int blockID) //returns quantity of blockid in inventory
 {
@@ -23,4 +26,5 @@ void clearInventory () //clears inventory
 	int i;
 	for (i = 0; i <= NUM_BLOCKS; i++)
 		inventory [i] = 0;
+	updateInvGraphics();
 }
