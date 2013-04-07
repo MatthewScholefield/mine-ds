a2009f6f4b187939a49b9c0954dc317466b25be5
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2013-04-07 07:04:07 +0000

    Survival/Creative mining cleanup. No need for all those if statements. No need for the boolean and tons of callbacks.

--- trunk/MineDS/source/inventory.cpp
+++ trunk/MineDS/source/inventory.cpp
@@ -5,17 +5,30 @@
 -----------------------------------------
 */
 #include "mining.h" //for NUM_BLOCKS
-#include "graphics\inventoryGraphics.h"
+#include "graphics/inventoryGraphics.h"
+#include "titlescreen.h"
 int inventory[NUM_BLOCKS];
 void addInventory(int blockID, int amount) //adds the specified amount to a blockvalue
 {
 	inventory [blockID] += amount;
 	updateInvGraphics();
 }
-void subInventory (int blockID, int amount) //subtracts the specified amount to a blockvalue
+void addInventory(int blockID)
 {
+	addInventory(blockID,1);
+}
+bool subInventory (int blockID, int amount) //subtracts the specified amount to a blockvalue
+{
+	if (isSurvival()==false) //Always return true in creative!
+		return true;
 	inventory [blockID] -= amount;
+	if (inventory[blockID] < 0)
+	{
+		inventory[blockID]+=amount;
+		return false;
+	}
 	updateInvGraphics();
+	return true;
 }
 int checkInventory (int blockID) //returns quantity of blockid in inventory
 {
