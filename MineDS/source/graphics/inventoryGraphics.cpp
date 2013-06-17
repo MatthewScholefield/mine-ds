8807dbef6eba4dbb627d7e44f57d9ef82c997eb3
CoolAs <goldenscruff@gmail.com>
2013-06-17 08:33:45 +0000

    Goofy inventory graphics!

--- MineDS/source/graphics/inventoryGraphics.cpp
+++ MineDS/source/graphics/inventoryGraphics.cpp
@@ -5,28 +5,27 @@
 #include "graphics.h"
 #include "../mining.h"
 #include "../inventory.h"
-#define INV_SLOTS 40
 
-Graphic INV[INV_SLOTS];
+Graphic INV[NUM_INV_SPACES];
 
 bool empty = false; //Whether the inventory is empty
 int total;
-bool loadedInv[INV_SLOTS];
-int invID [INV_SLOTS];
+bool loadedInv[NUM_INV_SPACES];
+int invID [NUM_INV_SPACES];
 
-void initInvGraphics() //Draws Grey background
+void initInvGraphics() //Draws Grey background? Why do that here?, It should be done in a drawing funciton.
 {
-	int i, j;
+	/*int i, j;
 	for (i=0;i<=24;i++)
 		for (j=0;j<=31;j++)
-			setSubBgTile(j,i,92);
+			setSubBgTile(j,i,92);*/
 }
 
 void updateInvGraphics() //changes inventory graphics array invenotry slots in case an item is removed or added
 {
 	int i, j, b = 1;
 	total = 0;
-	for (i = 1; i <= INV_SLOTS; i++)
+	for (i = 1; i <= NUM_INV_SPACES; i++)
 	{
 		while (checkInventory(b) == 0 && b < NUM_BLOCKS+2)
 			b++;
@@ -37,7 +36,7 @@ void updateInvGraphics() //changes inventory graphics array invenotry slots in c
 			if (loadedInv[i])
 				unloadGraphic(&INV[i]);
 			loadGraphicSub(&INV[i],2,b);
-			//invID[i]=b;            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< CAUSES INV NOT TO DISPLAY
+			//invID[i]=b;            <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< CAUSES INV NOT TO DISPLAY????
 			loadedInv[i] = true;
 			b++;
 			total ++;
@@ -50,17 +49,23 @@ void drawInv() //Draws the items in the inventory
 	if (!empty)
 	{
 		int i,j,a;
-		j = 23; //start x
-		i = 23; //start y
-		for (a=1;a<=total;a++)
+		j = 8; //start x
+		i = 16; //start y
+		for (a=1;a<=NUM_INV_SPACES;a++)
 		{
-			showGraphic(&INV[a],j,i);
-			if (j<200)
-				j += 30; //increment x
+			//Show sprite image of block, -2 to show the snow top block
+			showGraphic(&INV[a],j,i-2,false,2);
+			//Show tile image of block
+			setSubBgTile(j/8,i/8,154);
+			setSubBgTile(j/8+1,i/8,154,H_FLIP);
+			setSubBgTile(j/8,i/8+1,154,V_FLIP);
+			setSubBgTile(j/8+1,i/8+1,154,BOTH_FLIP);
+			if (j<224)
+				j += 16; //increment x
 			else
 			{
-				j = 23; //Exact same as start x
-				i += 30; //increment y
+				j = 8; //Exact same as start x
+				i += 16; //increment y
 			}
 		}
 	}
--- MineDS/source/inventory.h
+++ MineDS/source/inventory.h
@@ -1,5 +1,17 @@
-void addInventory (int blockID, int amount);
+bool addInventory (int blockID, int amount);
 bool subInventory (int blockID, int amount);
 int checkInventory (int blockID);
 void clearInventory ();
 void addInventory(int blockID);
+#define NUM_INV_SPACES 32
+typedef struct
+{
+	int blockId=0;
+	int blockAmount =0;
+}InvBlock;
+typedef struct
+{
+	InvBlock blocks[32];
+//	InvBlock armour[4];
+	int hand;
+}Inventory;
