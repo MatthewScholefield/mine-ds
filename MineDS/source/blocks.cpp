dfbdd0301a5aa0af60516c565b505e71d3e9a99c
matthew3311999@gmail.com <matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2013-07-02 02:25:01 +0000

    For testing, ONLY. I tried to add length when a block is broken, but I am unsure as to the method of adding the delay. I setup a system where for each item, it has a durability, and the block will not be destroyed until 'setBlock()' has been called x amount of times. Currently nothing is destroyable, therefore this is for testing purposes ONLY.

--- MineDS/source/blocks.cpp
+++ MineDS/source/blocks.cpp
@@ -5,15 +5,33 @@
         -----------------------------------------
 */
 #include "blockID.h"
+#include "mining.h"
 #define sizeOfArray(x) (sizeof(x)/4)
 int walkThroughBlocks[]={AIR,YELLOW_FLOWER,RED_FLOWER,SNOW_TOP,TORCH,LADDER,SHRUB,TALL_GRASS};
 int renderBright[]={AIR,LOG,OAK_WOOD,BIRCH_WOOD,LEAF,YELLOW_FLOWER,RED_FLOWER,CACTUS,TORCH,REDWOOD_LEAF,GLASS,SNOW_TOP,SHRUB,TALL_GRASS};
 int lightSourceBlocks[]={TORCH,PUMPKIN_LIGHT,GLOWSTONE};
 int items[]={PORKCHOP_RAW,BEEF_RAW,LEATHER,PICKAXE_WOOD,PICKAXE_STONE,PICKAXE_IRON,PICKAXE_GOLD,PICKAXE_DIAMOND,COAL,IRON,GOLD,DIAMOND,STICK};
+int hardness[NUM_BLOCKS]; //Slot is ID number, negative number means tool
+/*
+----TOOL----
+-Negative value
+-Faster Mining = Farther from 0
+-Slower Mining = Closer to 0
+
+----Block---
+-Positive Value
+-Harder Object = Higher Value
+-Softer Object = Lower value (Min 0)
+
+	 DIRT ~ 1
+	STONE ~ 20
+TREE WOOD ~ 3
+*/
 
 #define NUM_OF_LIGHT_SOURCES 3
 //The Number is equal to 15 - minecraftlightemitvalue
 int sourceAmount[]={1,0,0};
+
 int isBlockWalkThrough(int blockID)
 {
         int i;
@@ -66,4 +84,26 @@ int getLightAmount(int blockID)
         {
                 if (lightSourceBlocks[i]==blockID) return sourceAmount[i];
         }
+}
+void initHardness()
+{
+	int i;
+	for (i=1;i<=NUM_BLOCKS;i++)
+		hardness[i]=1;
+	for (i=1;i<=NUM_BLOCKS;i++)
+		if (item(i))
+			hardness[i]=-2;
+	hardness[PICKAXE_GOLD]=-20;
+	hardness[PICKAXE_DIAMOND]=-16;
+	hardness[PICKAXE_IRON]=-10;
+	hardness[PICKAXE_STONE]=-6;
+	hardness[PICKAXE_WOOD]=-4;
+	hardness[STONE]=20;
+	hardness[BIRCH_WOOD]=3;
+	hardness[OAK_WOOD]=3;
+
+}
+int getHardness(int blockID)
+{
+	return hardness[blockID];
 }
