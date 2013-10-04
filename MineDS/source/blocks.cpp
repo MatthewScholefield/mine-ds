b28fc686b22fe58eb8a5c75440b67918697aa199
Matthew3311999@gmail.com <Matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2013-10-04 11:44:52 +0000

    Added backend of blockTypes (to categorize blocks). I also could not compile with any iPrintfs, so I changed it to printf, but There must be a difference, what is it?

--- MineDS/source/blocks.cpp
+++ MineDS/source/blocks.cpp
@@ -6,21 +6,43 @@
 */
 #include "blockID.h"
 #include "mining.h"
+#include <stdio.h>
+//#include <iostream>
+#include <initializer_list>
 #define sizeOfArray(x) (sizeof(x)/4)
+
+//Block Types
+#define SOIL 1 //Any type of Grass and Dirt
+#define WOOD 2
+
+//Tool Types
+#define PICKAXE -1
+#define SHOVEL -2
+#define AXE -3
+#define SWORD -4
+
 int walkThroughBlocks[]={AIR,YELLOW_FLOWER,RED_FLOWER,SNOW_TOP,TORCH,LADDER,SHRUB,TALL_GRASS};
 int renderBright[]={AIR,LOG,OAK_WOOD,BIRCH_WOOD,LEAF,YELLOW_FLOWER,RED_FLOWER,CACTUS,TORCH,REDWOOD_LEAF,GLASS,SNOW_TOP,SHRUB,TALL_GRASS};
 int lightSourceBlocks[]={TORCH,PUMPKIN_LIGHT,GLOWSTONE};
 int items[]={PORKCHOP_RAW,BEEF_RAW,LEATHER,PICKAXE_WOOD,PICKAXE_STONE,
 		PICKAXE_IRON,PICKAXE_GOLD,PICKAXE_DIAMOND,COAL,IRON,GOLD,DIAMOND,
-		STICK,FLESH,BEEF_COOKED,PORKCHOP_COOKED,CHICKEN_RAW,CHICKEN_COOKED, SHOVEL_DIAMOND, SHOVEL_WOOD, SHOVEL_STONE, SHOVEL_IRON, SHOVEL_GOLD, AXE_DIAMOND, AXE_WOODEN,AXE_STONE,AXE_IRON,AXE_GOLD,SWORD_DIAMOND,SWORD_WOOD,SWORD_STONE,SWORD_IRON,SWORD_GOLD};
+		STICK,FLESH,BEEF_COOKED,PORKCHOP_COOKED,CHICKEN_RAW,CHICKEN_COOKED, SHOVEL_DIAMOND, SHOVEL_WOOD, SHOVEL_STONE, SHOVEL_IRON, SHOVEL_GOLD, AXE_DIAMOND, AXE_WOOD,AXE_STONE,AXE_IRON,AXE_GOLD,SWORD_DIAMOND,SWORD_WOOD,SWORD_STONE,SWORD_IRON,SWORD_GOLD};
 int hardness[NUM_BLOCKS]; //Slot is ID number, negative number means tool
+int blockType[NUM_BLOCKS]; //Type of block/tool
+
+void setArray(int * array, std::initializer_list<size_t> slots, int setValue) {
+    for (auto i : slots) {
+        array[i] = setValue;
+    }
+}
 /*
-----TOOL----
+========HARDNESSS========
+---Tools----
 -Negative value
 -Faster Mining = Farther from 0
 -Slower Mining = Closer to 0
 
-----Block---
+---Blocks---
 -Positive Value
 -Harder Object = Higher Value
 -Softer Object = Lower value (Min 0)
@@ -28,6 +50,19 @@ int hardness[NUM_BLOCKS]; //Slot is ID number, negative number means tool
 	 DIRT ~ 1
 	STONE ~ 20
 TREE WOOD ~ 3
+
+==========TYPE==========
+Only used for block category,
+NOT for block properties (WalkThrough)
+otherwise values could overlap
+
+-----Tools------
+-Negative Value-
+----------------
+
+-----Blocks-----
+-Positive Value-
+----------------
 */
 
 #define NUM_OF_LIGHT_SOURCES 3
@@ -95,15 +130,35 @@ void initHardness()
 	for (i=1;i<=NUM_BLOCKS;i++)
 		if (item(i))
 			hardness[i]=-2;
-	hardness[PICKAXE_GOLD]=-20;
-	hardness[PICKAXE_DIAMOND]=-16;
-	hardness[PICKAXE_IRON]=-10;
-	hardness[PICKAXE_STONE]=-6;
-	hardness[PICKAXE_WOOD]=-4;
+	setArray(hardness,{PICKAXE_GOLD,AXE_GOLD,SHOVEL_GOLD},-20);
+	setArray(hardness,{PICKAXE_DIAMOND,AXE_DIAMOND,SHOVEL_DIAMOND},-16);
+	setArray(hardness,{PICKAXE_IRON,AXE_IRON,SHOVEL_IRON},-10);
+	setArray(hardness,{PICKAXE_STONE,AXE_STONE,SHOVEL_STONE},-6);
+	setArray(hardness,{PICKAXE_WOOD,AXE_WOOD,SHOVEL_WOOD},-4);
+
 	hardness[STONE]=20;
+	hardness[SANDSTONE]=13;
 	hardness[BIRCH_WOOD]=3;
 	hardness[OAK_WOOD]=3;
+	hardness[JUNGLE_WOOD]=3;
 
+
+}
+void initType()
+{
+	//Tools
+	setArray(blockType,{PICKAXE_STONE, PICKAXE_IRON, PICKAXE_GOLD, PICKAXE_WOOD},PICKAXE); //Pickaxes
+	setArray(blockType,{SHOVEL_STONE, SHOVEL_IRON, SHOVEL_GOLD, SHOVEL_WOOD},SHOVEL); //Shovels
+	setArray(blockType,{AXE_STONE, AXE_IRON, AXE_GOLD, AXE_WOOD},AXE); //Axes
+	setArray(blockType,{SWORD_STONE, SWORD_IRON, SWORD_GOLD, SWORD_WOOD},SWORD); //Swords
+
+	//Blocks
+	setArray(blockType,{JUNGLE_WOOD,BIRCH_WOOD},WOOD);
+	setArray(blockType,{JUNGLE_GRASS,GRASS,DIRT},SOIL);
+}
+int getType(int blockID)
+{
+	return blockType[blockID];
 }
 int getHardness(int blockID)
 {
