b8b6a7404b022c7be27a15828d42d3ef8f2e3fa4
CoolAs <goldenscruff@gmail.com>
2013-10-05 04:40:08 +0000

    Created a C only version of setArray. It uses va_lists which are also used in iprintf so no library overhead should be added ;)

--- MineDS/source/blocks.cpp
+++ MineDS/source/blocks.cpp
@@ -6,21 +6,50 @@
 */
 #include "blockID.h"
 #include "mining.h"
+#include <stdio.h>
+#include <stdarg.h>
+//#include <iostream>
+//#include <initializer_list>
 #define sizeOfArray(x) (sizeof(x)/4)
+
+//Block Types
+#define SOIL 1 //Any type of Grass and Dirt
+#define WOOD 2
+
+//Tool Types
+#define PICKAXE -1
+#define SHOVEL -2
+#define AXE -3
+#define SWORD -4
+
 int walkThroughBlocks[]={AIR,YELLOW_FLOWER,RED_FLOWER,SNOW_TOP,TORCH,LADDER,SHRUB,TALL_GRASS};
 int renderBright[]={AIR,LOG,OAK_WOOD,BIRCH_WOOD,LEAF,YELLOW_FLOWER,RED_FLOWER,CACTUS,TORCH,REDWOOD_LEAF,GLASS,SNOW_TOP,SHRUB,TALL_GRASS};
 int lightSourceBlocks[]={TORCH,PUMPKIN_LIGHT,GLOWSTONE};
 int items[]={PORKCHOP_RAW,BEEF_RAW,LEATHER,PICKAXE_WOOD,PICKAXE_STONE,
 		PICKAXE_IRON,PICKAXE_GOLD,PICKAXE_DIAMOND,COAL,IRON,GOLD,DIAMOND,
-		STICK,FLESH,BEEF_COOKED,PORKCHOP_COOKED,CHICKEN_RAW,CHICKEN_COOKED, SHOVEL_DIAMOND, SHOVEL_WOOD, SHOVEL_STONE, SHOVEL_IRON, SHOVEL_GOLD, AXE_DIAMOND, AXE_WOODEN,AXE_STONE,AXE_IRON,AXE_GOLD,SWORD_DIAMOND,SWORD_WOOD,SWORD_STONE,SWORD_IRON,SWORD_GOLD};
+		STICK,FLESH,BEEF_COOKED,PORKCHOP_COOKED,CHICKEN_RAW,CHICKEN_COOKED, SHOVEL_DIAMOND, SHOVEL_WOOD, SHOVEL_STONE, SHOVEL_IRON, SHOVEL_GOLD, AXE_DIAMOND, AXE_WOOD,AXE_STONE,AXE_IRON,AXE_GOLD,SWORD_DIAMOND,SWORD_WOOD,SWORD_STONE,SWORD_IRON,SWORD_GOLD};
 int hardness[NUM_BLOCKS]; //Slot is ID number, negative number means tool
+int blockType[NUM_BLOCKS]; //Type of block/tool
+
+void setArray(int * array, int setValue, int numOfItems, ...)
+{
+	//va_list is fine to use and it shouldn't add any overhead as it is used in (i)printf to create infinite arguments.
+	va_list slots;
+	va_start(slots,numOfItems);
+	for (int a = 0; a < numOfItems; a++)
+	{
+		array[va_arg(slots,int)] = setValue;
+	}
+	va_end(slots);
+}
 /*
-----TOOL----
+========HARDNESSS========
+---Tools----
 -Negative value
 -Faster Mining = Farther from 0
 -Slower Mining = Closer to 0
 
-----Block---
+---Blocks---
 -Positive Value
 -Harder Object = Higher Value
 -Softer Object = Lower value (Min 0)
@@ -28,6 +57,19 @@ int hardness[NUM_BLOCKS]; //Slot is ID number, negative number means tool
 	 DIRT ~ 1
 	STONE ~ 20
 TREE WOOD ~ 3
+
+==========TYPE==========
+Only used for block category,
+NOT for block properties (WalkThrough)
+otherwise values could overlap
+
+-----Tools------
+-Negative Value-
+----------------
+
+-----Blocks-----
+-Positive Value-
+----------------
 */
 
 #define NUM_OF_LIGHT_SOURCES 3
@@ -95,15 +137,35 @@ void initHardness()
 	for (i=1;i<=NUM_BLOCKS;i++)
 		if (item(i))
 			hardness[i]=-2;
-	hardness[PICKAXE_GOLD]=-20;
-	hardness[PICKAXE_DIAMOND]=-16;
-	hardness[PICKAXE_IRON]=-10;
-	hardness[PICKAXE_STONE]=-6;
-	hardness[PICKAXE_WOOD]=-4;
+	setArray(hardness,-20,3,PICKAXE_GOLD,AXE_GOLD,SHOVEL_GOLD);
+	setArray(hardness,-16,3,PICKAXE_DIAMOND,AXE_DIAMOND,SHOVEL_DIAMOND);
+	setArray(hardness,-10,3,PICKAXE_IRON,AXE_IRON,SHOVEL_IRON);
+	setArray(hardness, -6,3,PICKAXE_STONE,AXE_STONE,SHOVEL_STONE);
+	setArray(hardness, -4,3,PICKAXE_WOOD,AXE_WOOD,SHOVEL_WOOD);
+
 	hardness[STONE]=20;
+	hardness[SANDSTONE]=13;
 	hardness[BIRCH_WOOD]=3;
 	hardness[OAK_WOOD]=3;
+	hardness[JUNGLE_WOOD]=3;
 
+
+}
+void initType()
+{
+	//Tools
+	setArray(blockType,PICKAXE,4,PICKAXE_STONE, PICKAXE_IRON, PICKAXE_GOLD, PICKAXE_WOOD); //Pickaxes
+	setArray(blockType,SHOVEL,4,SHOVEL_STONE, SHOVEL_IRON, SHOVEL_GOLD, SHOVEL_WOOD); //Shovels
+	setArray(blockType,AXE,4,AXE_STONE, AXE_IRON, AXE_GOLD, AXE_WOOD); //Axes
+	setArray(blockType,SWORD,4,SWORD_STONE, SWORD_IRON, SWORD_GOLD, SWORD_WOOD); //Swords
+
+	//Blocks
+	setArray(blockType,WOOD,2,JUNGLE_WOOD,BIRCH_WOOD);
+	setArray(blockType,SOIL,3,JUNGLE_GRASS,GRASS,DIRT);
+}
+int getType(int blockID)
+{
+	return blockType[blockID];
 }
 int getHardness(int blockID)
 {
