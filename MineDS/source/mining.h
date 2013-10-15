3825531eba58395c383a5f904c49abf7dce499fa
Matthew Scholefield <matthew3311999@gmail.com>
2013-10-15 00:38:02 +0000

    Added Mushroom biomes and fixed a few bugs

--- MineDS/source/mining.h
+++ MineDS/source/mining.h
@@ -1,6 +1,6 @@
 #include "world.h"
 #include <nds.h>
-#define NUM_BLOCKS 91
+#define NUM_BLOCKS 99
 int getSelectedblock();
 void miningUpdate(worldObject* world,int sx,int sy,touchPosition touch, int keys);
 void blocksCanPlace();
