da093b8ef0a13be5d2bf100df746688fe0685788
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2013-11-12 10:15:17 +0000

    The crafting menu now shows up, and can tell you what blocks you can craft with what! :)

--- MineDS/source/crafting.h
+++ MineDS/source/crafting.h
@@ -11,4 +11,5 @@ void addCraftingRecipe(int block,int amount,int aid,int aamount,int bid,int bamo
 void addCraftingRecipe(int block,int amount,int aid,int aamount,int bid,int bamount,int cid,int camount);
 void addCraftingRecipe(int block,int amount,int aid,int aamount,int bid,int bamount);
 void addCraftingRecipe(int block,int amount,int aid,int aamount);
-
+void craftingMenuInit();
+int craftingMenuUpdate();
--- MineDS/source/sounds.cpp
+++ MineDS/source/sounds.cpp
@@ -7,11 +7,14 @@
 #include <stdio.h>
 #include <dswifi9.h>
 #include <string.h>
+//#define SOUNDS //Comment out for no sounds
+
 bool maxModInit=false;
 musicStruct music;
 int last_music = -1;
 void initSounds()
 {
+#ifdef SOUNDS
   	mmInitDefaultMem( (mm_addr)soundbank_bin );
 	int i;
 	for (i=0;i<MSL_NSAMPS;i++) mmLoadEffect(i); //loads all sfx
@@ -22,9 +25,12 @@ void initSounds()
 	music.frames=0;
 	music.volumechanging=0; //0 for no changing, 1 for fade in, 2 for fade out..
 	maxModInit=true;
+#endif
 }
 void playSound(int sound)
 {
+
+#ifdef SOUNDS
 	if (isWifi())
 	{
 		unsigned short buffer[100];
@@ -42,9 +48,12 @@ void playSound(int sound)
 		case COW_H: mmEffect(SFX_COWHIT); break;
 		case SHEEP_H: mmEffect(SFX_SHEEPHIT); break;
 	}
+#endif
 }
 void playSoundNiFi(int sound)
 {
+
+#ifdef SOUNDS
 	if (maxModInit==false) 
 		initSounds();
 	switch(sound)
@@ -54,10 +63,12 @@ void playSoundNiFi(int sound)
 		case ZOMBIE_H: mmEffect(SFX_ZOMBIEHIT); break;
 		case COW_H: mmEffect(SFX_COWHIT); break;
 	}
+#endif
 }
 
 void playMusic(int music)
 {
+#ifdef SOUNDS
 	if (maxModInit==false)
 		initSounds();
 	last_music = music;	
@@ -66,10 +77,12 @@ void playMusic(int music)
 		case CALM : mmLoad(MOD_CALM); mmStart(MOD_CALM,MM_PLAY_ONCE); break;
 		case HAL2 : mmLoad(MOD_HAL2); mmStart(MOD_HAL2,MM_PLAY_LOOP); break;
 	}
+#endif
 }
 
 void stopMusic()
 {
+#ifdef SOUNDS
 	mmStop();	
 	if (last_music == -1)
 		return;
@@ -79,6 +92,7 @@ void stopMusic()
 		case HAL2 : mmUnload(MOD_HAL2); break;
 	}
 	last_music = -1;
+#endif
 }
 /*void soundUpdate(){
 	music.frames++;
