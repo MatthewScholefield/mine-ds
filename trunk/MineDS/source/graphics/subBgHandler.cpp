1a5427e674f662b8d69707ad83197804da44b13c
Matthew3311999@gmail.com <Matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2014-06-25 12:38:52 +0000

    Fixed button right edge, and made the controls screen much better and easier. I also restructured some of the titlescreen functions into a bool rather than redrawing them.

--- trunk/MineDS/source/graphics/subBgHandler.cpp
+++ trunk/MineDS/source/graphics/subBgHandler.cpp
@@ -6,44 +6,48 @@ uint16 *fontgfx;
 #define V_FLIP 2
 #define H_FLIP 1
 #define BOTH_FLIP 3
-inline void setSubTileXY(int x, int y, uint16 tile,int palette,int flip)
+
+inline void setSubTileXY(int x, int y, uint16 tile, int palette, int flip)
 {
-	tile |= palette<<12;
-	tile |= flip<<10;
+	tile |= palette << 12;
+	tile |= flip << 10;
 	//bgptr[(x%64) + (y%64)*64] = tile;
-	bgptr[x + y*32] = tile;
+	bgptr[x + y * 32] = tile;
 }
-void setSubBgTile(int x,int y,int tile)
+
+void setSubBgTile(int x, int y, int tile)
 {
-	const int palette=0;
+	const int palette = 0;
 	//x=x*2; //Convert tiles of 16 to tiles of 8
 	//y=y*2;
 	//tile=tile*4;
 	//Draw the 4 (8x8) tiles
-	setSubTileXY(x,y,tile,palette,0);
+	setSubTileXY(x, y, tile, palette, 0);
 	//setSubTileXY(x+1,y,tile+2,palette);
 	//setSubTileXY(x,y+1,tile+1,palette);
 	//setSubTileXY(x+1,y+1,tile+3,palette);
 }
-void setSubBgTile(int x,int y,int tile,int flip)
+
+void setSubBgTile(int x, int y, int tile, int flip)
 {
-	const int palette=0;
+	const int palette = 0;
 	//x=x*2; //Convert tiles of 16 to tiles of 8
 	//y=y*2;
 	//tile=tile*4;
 	//Draw the 4 (8x8) tiles
-	setSubTileXY(x,y,tile,palette,flip);
+	setSubTileXY(x, y, tile, palette, flip);
 	//setSubTileXY(x+1,y,tile+2,palette);
 	//setSubTileXY(x,y+1,tile+1,palette);
 	//setSubTileXY(x+1,y+1,tile+3,palette);
 }
+
 void subBgInit()
 {
-	videoSetModeSub(MODE_5_2D | DISPLAY_BG_EXT_PALETTE);	
-	vramSetBankC(VRAM_C_SUB_BG); 
-	int bg = bgInitSub(2,  BgType_ExRotation, BgSize_ER_256x256, 1,6); //16bit BG
-	bgptr=bgGetMapPtr(bg);
-	fontgfx=(uint16*)0x0620400;
+	videoSetModeSub(MODE_5_2D | DISPLAY_BG_EXT_PALETTE);
+	vramSetBankC(VRAM_C_SUB_BG);
+	int bg = bgInitSub(2, BgType_ExRotation, BgSize_ER_256x256, 1, 6); //16bit BG
+	bgptr = bgGetMapPtr(bg);
+	fontgfx = (uint16*) 0x0620400;
 	vramSetBankH(VRAM_H_LCD);
 	dmaCopy(&fontPal, VRAM_H_EXT_PALETTE[0][0], fontPalLen); //Copy the palette
 	dmaCopy(&sub_bgPal, VRAM_H_EXT_PALETTE[2][0], sub_bgPalLen); //Copy the palette
@@ -51,35 +55,114 @@ void subBgInit()
 	dmaCopy(&sub_bgTiles, bgGetGfxPtr(bg), sub_bgTilesLen);
 	dmaCopy(&fontTiles, fontgfx, fontTilesLen);
 }
-void drawButton(int x,int y, int sizex)
+
+void drawButton(int x, int y, int sizex)
 {
+	setSubBgTile(x, y + 1, 27);
+	setSubBgTile(x, y, 26);
+	setSubBgTile(x, y + 2, 26, V_FLIP);
+	setSubBgTile(x + sizex, y + 1, 27, H_FLIP);
+	setSubBgTile(x + sizex, y, 26, H_FLIP);
+	setSubBgTile(x + sizex, y + 2, 26, BOTH_FLIP);
+
 	int i;
-	setSubBgTile(x,y,26);
-	setSubBgTile(x,y+2,26,V_FLIP);
-	setSubBgTile(x+sizex,y,26,H_FLIP);
-	setSubBgTile(x+sizex,y+2,26,BOTH_FLIP);
-	for(i=0;i<=sizex-1;i++)
-		setSubBgTile(x+1+i,y,30);
-	for(i=0;i<=sizex-1;i++)
-		setSubBgTile(x+1+i,y+2,30,V_FLIP);
-	setSubBgTile(x,y+1,27);
-	setSubBgTile(x+sizex,y+1,27,H_FLIP);
-	for (i=0;i<=sizex-1;i++)
-		setSubBgTile(x+1+i,y+1,28 + (i%2));
+	for (i = 0; i < sizex - 1; i++)
+		setSubBgTile(x + 1 + i, y, 30);
+	for (i = 0; i < sizex - 1; i++)
+		setSubBgTile(x + 1 + i, y + 2, 30, V_FLIP);
+	for (i = 0; i < sizex - 1; i++)
+		setSubBgTile(x + 1 + i, y + 1, 28 + (i % 2));
 }
-void drawButtonColored(int x,int y, int sizex)
+
+void drawButtonColored(int x, int y, int sizex)
 {
+	setSubBgTile(x, y, 58);
+	setSubBgTile(x, y + 2, 58, V_FLIP);
+	setSubBgTile(x + sizex, y, 58, H_FLIP);
+	setSubBgTile(x + sizex, y + 2, 58, BOTH_FLIP);
+	setSubBgTile(x, y + 1, 59);
+	setSubBgTile(x + sizex, y + 1, 59, H_FLIP);
+
 	int i;
-	setSubBgTile(x,y,58);
-	setSubBgTile(x,y+2,58,V_FLIP);
-	setSubBgTile(x+sizex,y,58,H_FLIP);
-	setSubBgTile(x+sizex,y+2,58,BOTH_FLIP);
-	for(i=0;i<=sizex-1;i++)
-		setSubBgTile(x+1+i,y,62);
-	for(i=0;i<=sizex-1;i++)
-		setSubBgTile(x+1+i,y+2,62,V_FLIP);
-	setSubBgTile(x,y+1,59);
-	setSubBgTile(x+sizex,y+1,59,H_FLIP);
-	for(i=0;i<=sizex-1;i++)
-		setSubBgTile(x+1+i,y+1,60 + (i%2));
+	for (i = 0; i < sizex - 1; i++)
+		setSubBgTile(x + 1 + i, y, 62);
+	for (i = 0; i < sizex - 1; i++)
+		setSubBgTile(x + 1 + i, y + 2, 62, V_FLIP);
+	for (i = 0; i < sizex - 1; i++)
+		setSubBgTile(x + 1 + i, y + 1, 60 + (i % 2));
+}
+
+void drawBox(int x, int y, int lx, int ly)
+{ //Todo: Change switch statement back to if/else for readability
+	for (int i = 0; i < lx; i++)
+	{
+
+		for (int j = 0; j < ly; j++)
+		{
+			switch (i)
+			{
+				case 0:
+				{
+					switch (j)
+					{
+						case 0: //Top-Left Corner
+							setSubBgTile(x + i, y + j, 26);
+							break;
+						default:
+							if (j == ly - 1)//Bottom-Left Corner
+								setSubBgTile(x + i, y + j, 26, V_FLIP);
+							else//Left Edge
+								setSubBgTile(x + i, y + j, 27);
+							break;
+					}
+					break;
+				}
+				default:
+				{
+					if (i == lx - 1)
+					{
+						switch (j)
+						{
+							case 0: //Top-Right Corner
+								setSubBgTile(x + i, y + j, 26, H_FLIP);
+								break;
+							default:
+								if (j == ly - 1)//Bottom-Right Corner
+									setSubBgTile(x + i, y + j, 26, BOTH_FLIP);
+								else//Right Edge
+									setSubBgTile(x + i, y + j, 27, H_FLIP);
+								break;
+						}
+					}
+					else
+					{
+						switch (j)
+						{
+							case 0: //Top Edge
+								setSubBgTile(x + i, y + j, 30);
+								break;
+							default:
+								if (j == ly - 1)//Bottom Edge
+									setSubBgTile(x + i, y + j, 30, V_FLIP);
+								else//Center
+									setSubBgTile(x + i, y + j, 28 + i % 2);
+								break;
+						}
+					}
+				}
+			}
+
+		}
+	}
+}
+
+void drawBoxCenter(int x, int y, int lx, int ly) //Draws a box without borders
+{
+	for (int i = 0; i < lx; i++)
+	{
+		for (int j = 0; j < ly; j++)
+		{
+			setSubBgTile(x + i, y + j, 28 + (i + 1) % 2);
+		}
+	}
 }
