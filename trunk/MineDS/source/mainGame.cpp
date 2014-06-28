674546a1f1c501a06cdf7aeae232b8917c218e47
Matthew3311999@gmail.com <Matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2014-06-28 23:20:14 +0000

    Added visibility option to the Button class and made cactuses hurt mobs instantly

--- trunk/MineDS/source/mainGame.cpp
+++ trunk/MineDS/source/mainGame.cpp
@@ -22,79 +22,80 @@ bool up;
 Graphic graphics[10];
 void mobHandlerUpdate(worldObject* world);
 
-worldObject* mainGame(int mode,worldObject* CurrentWorld)
+worldObject* mainGame(int mode, worldObject* CurrentWorld)
 {
 	consoleClear();
 	clear_messages();
 	if (mode != 3)// Generating world for preview doesn't require screen change
-            lcdMainOnBottom();
+		lcdMainOnBottom();
 	touchPosition touch;
 	initInvGraphics();
 
-	addInventory(PICKAXE_STONE,1);
-	addInventory(AXE_STONE,1);
-	addInventory(SHOVEL_STONE,1);
-	addInventory(PICKAXE_DIAMOND,1);
-	addInventory(AXE_DIAMOND,1);
-	addInventory(SHOVEL_DIAMOND,1);
+	addInventory(PICKAXE_STONE, 1);
+	addInventory(AXE_STONE, 1);
+	addInventory(SHOVEL_STONE, 1);
+	addInventory(PICKAXE_DIAMOND, 1);
+	addInventory(AXE_DIAMOND, 1);
+	addInventory(SHOVEL_DIAMOND, 1);
 
-	if (mode==0) // Generate new world and reset the camera and time
+	if (mode == 0) // Generate new world and reset the camera and time
 	{
 		printXY(1, 8, "Generating world...\n");
-		if (CurrentWorld==NULL) CurrentWorld = (worldObject *) calloc(1, sizeof(worldObject));
-		if (CurrentWorld==NULL)
+		if (CurrentWorld == NULL) CurrentWorld = (worldObject *) calloc(1, sizeof (worldObject));
+		if (CurrentWorld == NULL)
 			return NULL;
 		mobsReset();
 		generateWorld(CurrentWorld);
 		printXY(1, 8, "A");
-		CurrentWorld->CamX=0;
-		CurrentWorld->CamY=0;
-		CurrentWorld->timeInWorld=0;
+		CurrentWorld->CamX = 0;
+		CurrentWorld->CamY = 0;
+		CurrentWorld->timeInWorld = 0;
 		consoleClear();
 	}
-        //mode 1: Return to game
-        else if (mode==2) //Load World
-        {
-            printXY(1, 8, "Loading world...\n");
-            if (CurrentWorld==NULL) CurrentWorld = (worldObject *) calloc(1, sizeof(worldObject));
-            if (CurrentWorld==NULL)
-		return NULL;
-            mobsReset();
-            loadWorld(CurrentWorld);
-            printXY(1, 8, "A");
-            CurrentWorld->CamX=0;
-            CurrentWorld->CamY=0;
-            CurrentWorld->timeInWorld=0;
-            consoleClear();
-                 
-        }
-        else if (mode == 3)//Generate world preview and return
-        {
-		if (CurrentWorld==NULL) CurrentWorld = (worldObject *) calloc(1, sizeof(worldObject));
-		if (CurrentWorld==NULL)
-                    return NULL;
+		//mode 1: Return to game
+	else if (mode == 2) //Load World
+	{
+		printXY(1, 8, "Loading world...\n");
+		if (CurrentWorld == NULL) CurrentWorld = (worldObject *) calloc(1, sizeof (worldObject));
+		if (CurrentWorld == NULL)
+			return NULL;
+		mobsReset();
+		loadWorld(CurrentWorld);
+		printXY(1, 8, "A");
+		CurrentWorld->CamX = 0;
+		CurrentWorld->CamY = 0;
+		CurrentWorld->timeInWorld = 0;
+		consoleClear();
+
+	}
+	else if (mode == 3)//Generate world preview and return
+	{
+		if (CurrentWorld == NULL) CurrentWorld = (worldObject *) calloc(1, sizeof (worldObject));
+		if (CurrentWorld == NULL)
+			return NULL;
 		mobsReset();
 		generateSmallWorld(CurrentWorld);
-		CurrentWorld->CamX=0;
-		CurrentWorld->CamY=0;
-		CurrentWorld->timeInWorld=0;
-                mobHandlerUpdate(CurrentWorld);
-                worldRender_Render(CurrentWorld,CurrentWorld->CamX,CurrentWorld->CamY);
-                return CurrentWorld;
-        }
-        if (isSurvival())
-        {
-            drawButton(21,16,9);
-            drawButton(9,16,10);
-            printXY(22, 17, "Crafting");
-            printXY(10, 17, "Save Game");
-        }
-	while(1){ //Infinitely repeats until break
+		CurrentWorld->CamX = 0;
+		CurrentWorld->CamY = 0;
+		CurrentWorld->timeInWorld = 0;
+		mobHandlerUpdate(CurrentWorld);
+		worldRender_Render(CurrentWorld, CurrentWorld->CamX, CurrentWorld->CamY);
+		return CurrentWorld;
+	}
+	if (isSurvival())
+	{
+		drawButton(21, 16, 9);
+		drawButton(9, 16, 10);
+		printXY(22, 17, "Crafting");
+		printXY(10, 17, "Save Game");
+	}
+	while (1)
+	{ //Infinitely repeats until break
 		scanKeys();
 		touchRead(&touch);
-		miningUpdate(CurrentWorld,CurrentWorld->CamX,CurrentWorld->CamY,touch,keysDown());
+		miningUpdate(CurrentWorld, CurrentWorld->CamX, CurrentWorld->CamY, touch, keysDown());
 		mobHandlerUpdate(CurrentWorld);
-		updateInventory(&touch,CurrentWorld);
+		updateInventory(&touch, CurrentWorld);
 		update_message();
 		if (keysHeld() & KEY_B && keysHeld() & KEY_DOWN)
 			clear_messages();
@@ -105,7 +106,7 @@ worldObject* mainGame(int mode,worldObject* CurrentWorld)
 		oamUpdate(&oamSub);
 		graphicFrame();
 		timeUpdate(CurrentWorld);
-		worldRender_Render(CurrentWorld,CurrentWorld->CamX,CurrentWorld->CamY);
+		worldRender_Render(CurrentWorld, CurrentWorld->CamX, CurrentWorld->CamY);
 	}
 	return CurrentWorld;
 }
