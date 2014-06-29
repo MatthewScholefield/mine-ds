2dd97cf9f743ea378eb332049354916182554625
toaster234567@gmail.com <toaster234567@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2014-06-29 08:01:52 +0000

    We don't need two macro that do the same thing but are spelled differently.

--- MineDS/source/world.h
+++ MineDS/source/world.h
@@ -2,7 +2,6 @@
 #define WORLD_HEIGHT 128
 #define world_heightpx WORLD_HEIGHT*16
 #define world_widthpx WORLD_WIDTH*16
-#define WORLD_WIDTHpx WORLD_WIDTH*16
 #define WORLD_WIDTH 512
 typedef struct{
 	int blocks[WORLD_WIDTH+1][WORLD_HEIGHT+1];
