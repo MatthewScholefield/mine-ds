a7932de2fac1957be87f59bcea5358e01b4e20e9
Matthew Scholefield <matthew3311999@gmail.com>
2014-07-04 00:23:52 +0000

    Attempted to add control saving and loading. Anyone, please verify if it works. I also fixed a menu bug

--- MineDS/source/controls.h
+++ MineDS/source/controls.h
@@ -1,12 +1,6 @@
 #include <nds.h>
-#define ACTION_MOVE_LEFT 1
-#define ACTION_MOVE_RIGHT 2
-#define ACTION_JUMP 3
-#define ACTION_CROUCH 4
-#define ACTION_ITEM_LEFT 5
-#define ACTION_ITEM_RIGHT 6
-#define ACTION_SWITCH_SCREEN 7
-#define ACTION_MENU 8
-#define ACTION_CLIMB 9
-KEYPAD_BITS getKey(int action);
-void setKey(int action, KEYPAD_BITS key);
+
+KEYPAD_BITS getControlKey(int);
+void setControlKey(int, KEYPAD_BITS);
+void saveControlData(FILE* data);
+void loadControlData(FILE* data);
