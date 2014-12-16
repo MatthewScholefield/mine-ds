afeb21e976cb14853b20aed7310465c8c516b4c7
Matthew Scholefield <matthew3311999@gmail.com>
2014-12-16 23:08:16 +0000

    Fixed player not loading. Now worlds completely save, including player data.

--- MineDS/source/mobs/mobHandler.h
+++ MineDS/source/mobs/mobHandler.h
@@ -8,7 +8,7 @@ void mobHandlerInit();
 void mobHandlerUpdate(worldObject* world);
 void mobHandlerKillMob(int a);
 void mobHandlerReadWifiUpdate(int x,int y,int animation,int mobtype,int mobNum,worldObject* world,bool f);
-void mobsReset();
+void mobsReset(bool playerSpawned = false);
 void mobHandlerHurtMob(int mobNum,int amount, int type);
 int isMobAt(int x,int y);
 void mobHandlerHurtMobWifi(int mobNum,int amount, int type);
