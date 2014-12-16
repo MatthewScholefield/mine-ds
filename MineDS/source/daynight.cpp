3fb92971789ec321d88f71117c45feb79f841ab7
Matthew3311999@gmail.com <Matthew3311999@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2014-12-16 23:27:58 +0000

    Fixed time bug with new int in world object.

--- MineDS/source/daynight.cpp
+++ MineDS/source/daynight.cpp
@@ -6,29 +6,29 @@
 #include <string.h>
 
 #include "worldRender.h"
-int timeTillChange=0;
-int brightness=0;
-int r[15] = {16,16,15,14,13,12,11,8 ,8 ,7 ,6};
-int g[15] = {24,22,22,18,16,13,12,8 ,7 ,5 ,4};
-int b[15] = {31,31,30,29,28,27,26,26,24,23,22};
+int timeTillChange = 0;
+int r[15] = {16, 16, 15, 14, 13, 12, 11, 8, 8, 7, 6};
+int g[15] = {24, 22, 22, 18, 16, 13, 12, 8, 7, 5, 4};
+int b[15] = {31, 31, 30, 29, 28, 27, 26, 26, 24, 23, 22};
+
 void timeUpdate(worldObject* world)
 {
 	timeTillChange++;
-	if (timeTillChange>=300)
+	if (timeTillChange >= 3)
 	{
 		world->timeInWorld++;
-		timeTillChange=0;
-		if (world->timeInWorld>120) world->timeInWorld=0;
-		if (world->timeInWorld>=80 && world->timeInWorld<90) brightness++;
-		else if (world->timeInWorld>110) 				brightness--;	
-		setBackdropColor(RGB15(r[brightness],g[brightness],b[brightness]));		
-		setSun(brightness);	
+		timeTillChange = 0;
+		if (world->timeInWorld > 120) world->timeInWorld = 0;
+		if (world->timeInWorld >= 80 && world->timeInWorld < 90) world->worldBrightness++;
+		else if (world->timeInWorld > 110) world->worldBrightness--;
+		setBackdropColor(RGB15(r[world->worldBrightness], g[world->worldBrightness], b[world->worldBrightness]));
+		setSun(world->worldBrightness);
 		if (isWifi())
 		{
 			unsigned short buffer[100];
-			int server_id = getServerID();			
-			sprintf((char *)buffer,"[DNC: %d %d %d %d %d", server_id, brightness, r[brightness],g[brightness],b[brightness]);
-			Wifi_RawTxFrame(strlen((char *)buffer) + 1, 0x0014, buffer);	
+			int server_id = getServerID();
+			sprintf((char *) buffer, "[DNC: %d %d %d %d %d", server_id, world->worldBrightness, r[world->worldBrightness], g[world->worldBrightness], b[world->worldBrightness]);
+			Wifi_RawTxFrame(strlen((char *) buffer) + 1, 0x0014, buffer);
 		}
 	}
 }
