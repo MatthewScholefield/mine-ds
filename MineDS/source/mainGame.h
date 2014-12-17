070033cc21f990d5938ab38d8f7038a33eff4efb
toaster234567@gmail.com <toaster234567@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2014-12-17 22:17:22 +0000

    mainGame.cpp   - Separate mainGame() into small functions   - Fix mess of calloc's   - Add real in-game menu   - Move multiplayerGame.cpp here
    titlescreen.cpp
      - Fix back buttons
      - Add poweroff button
      - Move checking of gamemode to mainGame.ccp
      - General clean up
    
    world.h
      - Add gamemode_t enum
      - Remove unused variable
      - Change defines to uppercase

--- MineDS/source/mainGame.h
+++ MineDS/source/mainGame.h
@@ -1,2 +1,15 @@
+#ifndef _MAIN_GAME_H_
+#define _MAIN_GAME_H_
+
 #include "world.h"
-worldObject* mainGame(int Mode,worldObject* world);
+
+bool isCreative(void);
+bool isSurvival(void);
+void newGame(gamemode_t mode);
+bool loadGame(void);
+void previewScreen(int generate);
+void startGame(void);
+void startMultiplayerGame(bool host);
+void returnToGame(void);
+
+#endif /* !_MAIN_GAME_H_ */
