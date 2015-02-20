364d1bc5d6f95db974eb012a7bb7a64feefeb8cc
goldenscruff@gmail.com <goldenscruff@gmail.com@afe2457d-1bf5-3190-ee36-9cc5dbdb5743>
2015-02-20 10:42:30 +0000

    Grass now despawns when there is a block above it

---
+++ MineDS/source/blockupdaters/junglegrass.cpp
@@ -0,0 +1,31 @@
+#include "../world.h"
+#include "../general.h"
+#include "../graphics/graphics.h"
+#include "../blockupdate.h"
+#include <stdlib.h>
+#include "../blockID.h"
+#include "../blocks.h"
+#include "junglegrass.h"
+
+junglegrassUpdater::junglegrassUpdater()
+{
+  blockID = JUNGLE_GRASS;
+  chance = NO_CHANCE;
+}
+void junglegrassUpdater::update(worldObject* world,int x, int y, bool bg)
+{
+  if (bg == false)
+  {
+    if (isBlockWalkThrough(world->blocks[x][y-1]==false))
+    {
+      world->blocks[x][y]=DIRT;
+    }
+  }
+  else
+  {
+    if (isBlockWalkThrough(world->blocks[x][y-1]==false) || isBlockWalkThrough(world->bgblocks[x][y-1]==false))
+    {
+      world->bgblocks[x][y]=DIRT;
+    }
+  }
+}
