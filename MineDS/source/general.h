fc0c400126f30a89793f99791b524cb05916b6c3
Matthew Scholefield <matthew3311999@gmail.com>
2015-02-20 01:17:01 +0000

    Rewrote the message system in general.cpp. Messages now are removed over time and stack vertically.

--- MineDS/source/general.h
+++ MineDS/source/general.h
@@ -6,6 +6,9 @@
 
 void sleep(unsigned int seconds);
 void printGlobalMessage(const char* s);
+#define MESSAGE_COUNT 3
+#define MESSAGE_CLEAR_DELAY 500
+
 void update_message();
 void printLocalMessage(const char* s);
 void clear_messages();
