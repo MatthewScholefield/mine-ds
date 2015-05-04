0856173fde9b33d91e1b547c9dd567e4160055b7
Matthew Scholefield <matthew3311999@gmail.com>
2015-05-04 18:52:11 -0500

    Added the rest of the blocks to the block pages.

--- source/blockPages.h
+++ source/blockPages.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <nds.h>
-#define NUM_BLOCK_PAGES 5
+#define NUM_BLOCK_PAGES 6
 #define PAGE_WOOL 0
 #define PAGE_BLOCKS 1
-#define PAGE_PLANTS 2
-#define PAGE_ORES 3
-#define PAGE_TOOLS 4
+#define PAGE_CRAFTING 2
+#define PAGE_PLANTS 3
+#define PAGE_ORES 4
+#define PAGE_TOOLS 5
 
 void setBlockPage(int page);
 int getBlockPage();
