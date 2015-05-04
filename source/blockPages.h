497b1721885314b70d07ecc3f3e022d62a440656
Matthew Scholefield <matthew3311999@gmail.com>
2015-05-04 15:52:54 -0500

    Fixed Block Page names

--- source/blockPages.h
+++ source/blockPages.h
@@ -3,8 +3,9 @@
 #define NUM_BLOCK_PAGES 5
 #define PAGE_WOOL 0
 #define PAGE_BLOCKS 1
-#define PAGE_ORES 2
-#define PAGE_TOOLS 3
+#define PAGE_PLANTS 2
+#define PAGE_ORES 3
+#define PAGE_TOOLS 4
 
 void setBlockPage(int page);
 int getBlockPage();
