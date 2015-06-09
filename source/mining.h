b18189e209dfc99d42042986c8c9dc50cf9c8335
Matthew Scholefield <matthew3311999@gmail.com>
2015-06-09 00:19:01 -0500

    Mining bugfixes

--- source/mining.h
+++ source/mining.h
@@ -2,7 +2,7 @@
 #include "world.h"
 #include <nds.h>
 #define NUM_BLOCKS 102
-#define ACTIVATE_DELAY 30
+#define ACTIVATE_DELAY 20
 void miningUpdate(WorldObject* world, touchPosition touch);
 void enableMining();
 void setMiningDisabled(bool a);
