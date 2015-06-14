d5fbe44523d215b4837983b3e46ded046a765e40
Matthew Scholefield <matthew3311999@gmail.com>
2015-06-14 12:07:52 -0500

    Added texture pack UI I also adjusted how the camera is initialized to simplify world previewing
    Note:
    -Currently any file in the texture pack folder with any extension can be loaded
    -Any extension is removed for readability
    -Loading something that is not a texture pack will not crash the game, so I would consider it a feature (But a warning message should be implemented)

--- source/world.h
+++ source/world.h
@@ -52,7 +52,7 @@ public:
 	Furnace *furnaces[MAX_FURNACES];
 
 	WorldObject() : blocks { }, data{}, brightness{}, lightemit{}, sun{}, bgblocks{}
-	, camY(1), camX(1), timeInWorld(1), worldBrightness(0), gamemode(GAMEMODE_PREVIEW)
+	, camY(0), camX(0), timeInWorld(1), worldBrightness(0), gamemode(GAMEMODE_PREVIEW)
 	, seed(1), camCalcX(0.0), camCalcY(0.0), biome { }, chestInUse{}, chests{}, furnaces()
 	{
 	}
