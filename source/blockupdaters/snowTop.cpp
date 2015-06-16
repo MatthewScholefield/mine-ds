06b8d4831502f09640cf9b1f0f87dda705d4c806
Matthew Scholefield <matthew3311999@gmail.com>
2015-06-16 17:20:13 -0500

    Fixed snow top bug

--- source/blockupdaters/snowTop.cpp
+++ source/blockupdaters/snowTop.cpp
@@ -17,7 +17,7 @@ void snowTopUpdater::update(WorldObject* world, int x, int y, bool bg)
 	if (!bg && isBlockWalkThrough(world->blocks[x][y + 1]))
 	{
 		world->blocks[x][y] = AIR;
-		if (world->bgblocks[x][y + 1] == SNOW_GRASS || world->bgblocks[x][y + 1] == GRASS)
+		if ((world->bgblocks[x][y + 1] == SNOW_GRASS || world->bgblocks[x][y + 1] == GRASS) && world->bgblocks[x][y] == AIR)
 			world->bgblocks[x][y] = SNOW_TOP;
 	}
 	else if (bg && (!isBlockWalkThrough(world->blocks[x][y + 1]) || isBlockWalkThrough(world->bgblocks[x][y + 1])))
