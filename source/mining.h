bddae6fefcf194e5bc25cc12cf37f5579dec8f8c
Matthew Scholefield <matthew3311999@gmail.com>
2015-06-18 17:19:27 -0500

    Added block sounds for destroying and walking The project now uses nitroFS to store the soundbank The sound is played in stereo based on the destryo location

--- source/mining.h
+++ source/mining.h
@@ -3,6 +3,7 @@
 #include <nds.h>
 #define NUM_BLOCKS 102
 #define ACTIVATE_DELAY 20
+#define HIT_SOUND_DELAY 10
 void miningUpdate(WorldObject* world, touchPosition touch);
 void enableMining();
 void setMiningDisabled(bool a);
