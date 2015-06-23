57bda71c09a08370aff1b9e74df16740eebe098b
Matthew Scholefield <matthew3311999@gmail.com>
2015-06-23 07:46:05 -0500

    UI scrolling demo This should still function the same despite looking a bit awkward

--- source/fontHandler.cpp
+++ source/fontHandler.cpp
@@ -3,12 +3,14 @@
 ///@breif This function sets up the font and console used for iprintf("Hello World\n");.
 ///It took a while to get working with the subBgTiles, please don't modify unless you know what you are doing.
 ConsoleFont font;
+int consoleID;
 
 void setupFont()
 {
 	const int tile_base = 1;
 	const int map_base = 0;
 	PrintConsole *console = consoleInit(0, 0, BgType_Text8bpp, BgSize_T_256x256, map_base, tile_base, false, false);
+	consoleID = console->bgId;
 	font.gfx = (u16*) fontTiles;
 	font.pal = (u16*) fontPal;
 	font.numChars = 256;
@@ -17,6 +19,11 @@ void setupFont()
 	font.asciiOffset = 0;
 	font.convertSingleColor = false;
 	consoleSetFont(console, &font);
+}
+
+int getConsoleID()
+{
+	return consoleID;
 }
 
 void refreshFont()
