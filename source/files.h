6f34aa986f4faf9867698327528240a9ebf14291
CoolAs <goldenscruff@gmail.com>
2015-07-01 16:17:57 +1000

    Added option to build with FAT, NITRO, or both.

--- source/files.h
+++ source/files.h
@@ -2,11 +2,39 @@
 
 #include "world.h"
 #include "Config.h"
+#define NITRO_AND_FAT
+
+#ifdef NITRO_AND_FAT
+#define USE_FAT
+#define USE_NITRO
+#define SHOULD_SAVE true
+#define SHOULD_LOAD true
+#define SOUNDBANK_FILENAME "nitro:/soundbank.bin"
+#define SOUNDTRACK_FILENAME "nitro:/soundtrack.wav"
+#define MINE_DS_FOLDER "fat:/Mine DS/"
+#endif
+
+#ifdef JUST_NITRO
+#define USE_NITRO
+#define SHOULD_SAVE false
+#define SHOULD_LOAD false
+#define SOUNDBANK_FILENAME "nitro:/soundbank.bin"
+#define SOUNDTRACK_FILENAME "nitro:/soundtrack.wav"
+#define MINE_DS_FOLDER "nitro:/Mine DS/"
+#endif
+
+#ifdef JUST_FAT
+#define USE_FAT
+#define SHOULD_SAVE true
+#define SHOULD_LOAD true
+#define SOUNDBANK_FILENAME "fat:/Mine DS/soundbank.bin"
+#define SOUNDTRACK_FILENAME "fat:/Mine DS/soundtrack.wav"
+#define MINE_DS_FOLDER "fat:/Mine DS/"
+#endif
 
 #define WORLD_FILENAME	"MineWorld.dat"
 #define CONTROLS_FILENAME	"MineConfig.cfg"
 #define TEXTURE_FOLDER "Textures/"
-#define MINE_DS_FOLDER "/Mine DS/"
 
 void initFile(void);
 bool saveWorld(WorldObject *world);
