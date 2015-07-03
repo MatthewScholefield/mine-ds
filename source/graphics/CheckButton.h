85326113dc43150fdb5a5e5d4af36df93c2ee07c
Matthew Scholefield <matthew3311999@gmail.com>
2015-07-03 14:07:48 -0500

    Added proper check buttons
    In the process I fixed the following bugs:
    -Sub BG not moving the last pixel
    -Fonts not working past 32 tiles down
    -Build errors caused by uncommitted files
    -The Sub BG limiting vertical movement
    -The Menu not fully pulling up afterwards

---
+++ source/graphics/CheckButton.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "UIElement.h"
+#include <string.h>
+
+class CheckButton : public UIElement {
+private:
+	const int SIZE = 12;
+	const int OFF = 6;
+	bool enabled;
+	bool isTouching(int touchX, int touchY);
+	void drawCheck();
+public:
+	void setVisible(bool visible);
+	void draw(bool printLabel = true);
+	bool update(int state, int touchX, int touchY);
+
+	CheckButton(int x, int y, const char * const label, bool enabled, int length = -1, bool isVisible = true) :
+	UIElement(x, y, label, length < 0 ? strlen(label) + 5 : length, isVisible, nullptr), enabled(enabled) { }
+};
