0a964e8ec093e5de7e663331429c82ce25a5fbc7
CoolAs <goldenscruff@gmail.com>
2015-07-18 10:51:13 +1000

    Added WIP doors

---
+++ source/blockUpdaters/door.cpp
@@ -0,0 +1,93 @@
+#include "../world.h"
+#include "../general.h"
+#include "../graphics/graphics.h"
+#include "../blockUpdater.h"
+#include <stdlib.h>
+#include "../blockID.h"
+#include "../blocks.h"
+#include "door.h"
+#include "../mobs/hurt.h"
+#include "../mobs/mobHandler.h"
+
+DoorUpdater::DoorUpdater()
+{
+	blockID = DOOR_ITEM;
+	chance = NO_CHANCE;
+}
+
+void DoorUpdater::update(WorldObject* world, int x, int y, bool bg)
+{
+	int &blockXY = bg ? world->bgblocks[x][y] : world->blocks[x][y];
+	int &blockBelowXY = bg ? world->bgblocks[x][y + 1] : world->blocks[x][y + 1];
+	int &blockAboveXY = bg ? world->bgblocks[x][y - 1] : world->blocks[x][y - 1];
+	if (isBlockWalkThrough(blockBelowXY))
+	{
+		blockXY = AIR;
+		createItemMob(x, y, DOOR_ITEM);
+	}
+  else
+  {
+    blockXY = DOOR_OPEN_BOTTOM;
+    blockAboveXY = DOOR_OPEN_TOP;
+  }
+}
+
+void DoorUpdater::chanceUpdate(WorldObject* world, int x, int y, bool bg)
+{
+
+}
+
+
+
+DoorTopOpenUpdater::DoorTopOpenUpdater()
+{
+  blockID = DOOR_OPEN_TOP;
+  chance = NO_CHANCE;
+}
+DoorTopClosedUpdater::DoorTopClosedUpdater()
+{
+  blockID = DOOR_CLOSED_TOP;
+  chance = NO_CHANCE;
+}
+DoorBottomOpenUpdater::DoorBottomOpenUpdater()
+{
+  blockID = DOOR_OPEN_BOTTOM;
+  chance = NO_CHANCE;
+}
+DoorBottomClosedUpdater::DoorBottomClosedUpdater()
+{
+  blockID = DOOR_CLOSED_BOTTOM;
+  chance = NO_CHANCE;
+}
+void DoorTopUpdate(WorldObject* world, int x, int y, bool bg)
+{
+	int &blockXY = bg ? world->bgblocks[x][y] : world->blocks[x][y];
+	int &blockBelowXY = bg ? world->bgblocks[x][y + 1] : world->blocks[x][y + 1];
+	int &blockAboveXY = bg ? world->bgblocks[x][y - 1] : world->blocks[x][y - 1];
+  if (blockBelowXY==AIR)
+    blockXY = AIR;  
+}
+void DoorBottomUpdate(WorldObject* world, int x, int y, bool bg)
+{
+	int &blockXY = bg ? world->bgblocks[x][y] : world->blocks[x][y];
+	int &blockBelowXY = bg ? world->bgblocks[x][y + 1] : world->blocks[x][y + 1];
+	int &blockAboveXY = bg ? world->bgblocks[x][y - 1] : world->blocks[x][y - 1];
+  if (blockAboveXY==AIR)
+    blockXY = AIR;  
+}
+void DoorTopOpenUpdater::update(WorldObject* world, int x, int y, bool bg)
+{
+  DoorTopUpdate(world,x,y,bg);
+}
+void DoorTopClosedUpdater::update(WorldObject* world, int x, int y, bool bg)
+{
+  DoorTopUpdate(world,x,y,bg);
+}
+void DoorBottomOpenUpdater::update(WorldObject* world, int x, int y, bool bg)
+{
+  DoorBottomUpdate(world,x,y,bg);
+}
+void DoorBottomClosedUpdater::update(WorldObject* world,int x, int y, bool bg)
+{
+  DoorBottomUpdate(world,x,y,bg);
+}
---
+++ source/blockUpdaters/door.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "../world.h"
+#include "../blockUpdater.h"
+
+class DoorUpdater : public BlockUpdater
+{
+public:
+	DoorUpdater();
+	virtual void update(WorldObject* world, int x, int y, bool bg);
+	virtual void chanceUpdate(WorldObject* world, int x, int y, bool bg);
+};
+
+class DoorTopOpenUpdater : public BlockUpdater
+{
+  public:
+   DoorTopOpenUpdater();
+   virtual void update(WorldObject* world, int x, int y, bool bg);
+};
+class DoorTopClosedUpdater : public BlockUpdater
+{
+  public:
+   DoorTopClosedUpdater();
+   virtual void update(WorldObject* world, int x, int y, bool bg);
+};
+class DoorBottomOpenUpdater : public BlockUpdater
+{
+  public:
+   DoorBottomOpenUpdater();
+   virtual void update(WorldObject* world, int x, int y, bool bg);
+};
+class DoorBottomClosedUpdater : public BlockUpdater
+{
+  public:
+   DoorBottomClosedUpdater();
+   virtual void update(WorldObject* world, int x, int y, bool bg);
+};
