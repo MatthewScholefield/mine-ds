67d1680d6360c7368fca7276a75dd744dbcc8112
Wolfgange3311999 <matthew3311999@gmail.com>
2015-07-27 18:06:27 -0500

    Block Sprite lighting WIP

--- source/worldRender.h
+++ source/worldRender.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "world.h"
 #include "block_small.h"
+#include "graphics/graphics.h"
 
 void worldRender_Init();
 void worldRender_Render(WorldObject* world, int screen_x, int screen_y);
@@ -14,3 +15,22 @@ void updateBrightnessAround(WorldObject* world, int x, int y);
 void worldRender_LoadSprites();
 void setSun(int brightness);
 int getBrightness(WorldObject* world, int x, int y);
+void clearUnusedBlockSprites();
+
+class BlockSpriteContainer
+{
+public:
+	Graphic sprite;
+	int blockID;
+	bool hasBeenRendered;
+	void draw(int x, int y);
+	BlockSpriteContainer():sprite(),blockID(0),hasBeenRendered(false){}
+	BlockSpriteContainer(int blockID, int paletteID):sprite(),blockID(blockID),hasBeenRendered(false)
+	{
+		loadGraphic(&sprite, GRAPHIC_BLOCK,blockID,16,16,paletteID);
+	}
+	~BlockSpriteContainer()
+	{
+		unloadGraphic(&sprite);
+	}
+};
