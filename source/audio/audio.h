a0c67a447d1b39a17ece574d135e31a1fa77afb1
CoolAs <goldenscruff@gmail.com>
2015-07-31 19:36:30 +1000

    Added ADPCM fixes

--- source/audio/audio.h
+++ source/audio/audio.h
@@ -1,14 +1,18 @@
 #pragma once
 #include <nds.h>
 #include <stdio.h>
-
+#define WAV_BUFFER_SIZE 4096
 typedef struct
 {
 	int sampleRate;
 	int blockAlign;
 	int dataSize;
+	char* buffer;
 } WaveInfo;
-
+extern "C"
+{
+void ADCMReset();
 int parseWave(FILE* f, WaveInfo* w);
 s16 getADCM(FILE* f, WaveInfo* w);
-void ADCMReset();
+char getCharacter(FILE* f, WaveInfo* w);
+}
