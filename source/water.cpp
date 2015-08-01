1a9e2ebdbf004ad8384c9d8e5499d5b3df2067ad
CoolAs <goldenscruff@gmail.com>
2015-08-01 15:50:31 +1000

    Triangle is displayed at around 20% transparency

--- source/water.cpp
+++ source/water.cpp
@@ -1,22 +1,22 @@
 #include <nds.h>
-
+#define 	BLEND_CR   (*(vuint16*)0x04000050)
+#define 	BLEND_AB   (*(vuint16*)0x04000052)
+#define 	BLEND_Y   (*(vuint16*)0x04000054)
 float rotateX = 0.0;
 float rotateY = 0.0;
 
 void init3D()
 {
-	//set mode 0, enable BG0 and set it to 3D
-	//videoSetMode(MODE_0_3D);
-
 	// initialize gl
 	glInit();
-
+  BLEND_AB = 0x1010;
+  BLEND_CR = BLEND_ALPHA | BLEND_SRC_BG0 |BLEND_DST_BG2;	
 	// enable antialiasing
-	glEnable(GL_BLEND | GL_ALPHA_TEST); //DISP3DCNT_ENUM
-	glAlphaFunc(10);
-
+	glEnable(GL_BLEND); //DISP3DCNT_ENUM
+ 
 	// setup the rear plane
 	glClearColor(0, 0, 0, 0); // BG must be opaque for AA to work
+  glClearPolyID(3);
 	glClearDepth(0x7FFF);
 
 	//this should work the same as the normal gl call
@@ -30,7 +30,7 @@ void init3D()
 	gluLookAt(0.0, 0.0, 1.0, //camera possition
 			0.0, 0.0, 0.0, //look at
 			0.0, 1.0, 0.0); //up
-	glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE); //*/
+	glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE | POLY_ID(2)); //*/
 }
 
 void update3D()
@@ -49,7 +49,7 @@ void update3D()
 
 
 	//not a real gl function and will likely change
-	glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE);
+	glPolyFmt(POLY_ALPHA(7) | POLY_CULL_NONE | POLY_ID(4));
 
 
 	u16 keys = keysHeld();
