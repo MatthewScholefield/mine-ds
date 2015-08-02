cdb44192a5aa4fb719b7db48d7c658b23839d67b
Wolfgange3311999 <matthew3311999@gmail.com>
2015-08-02 02:19:52 -0500

    Added function to draw triangles by adding them to a vector
    In the process I added a Pair3 template

--- source/water.h
+++ source/water.h
@@ -1,3 +1,18 @@
 #pragma once
+void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3);
 void init3D();
 void update3D();
+
+template <class T>
+class Pair3
+{
+public:
+	T elements[3];
+
+	Pair3(T a, T b, T c)
+	{
+		elements[0] = a;
+		elements[1] = b;
+		elements[2] = c;
+	}
+};
