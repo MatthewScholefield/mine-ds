b672d16cafb85a5f4cfea690b98e2c3ef6e9bf64
Wolfgange3311999 <matthew3311999@gmail.com>
2015-08-02 02:19:52 -0500

    Added function to draw triangles by adding them to a vector
    In the process I added a Pair3 template

--- source/water.cpp
+++ source/water.cpp
@@ -1,7 +1,17 @@
 #include <nds.h>
+#include <vector>
+#include "water.h"
 #define 	BLEND_CR   (*(vuint16*)0x04000050)
 #define 	BLEND_AB   (*(vuint16*)0x04000052)
 #define 	BLEND_Y   (*(vuint16*)0x04000054)
+
+std::vector<Pair3<std::pair<int, int> > > triangles;
+
+void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+	triangles.emplace_back(Pair3<std::pair<int, int> >(std::pair<int, int>(x1, y1), std::pair<int, int>(x2, y2), std::pair<int, int>(x3, y3)));
+}
+
 void init3D()
 {
 	// initialize gl
@@ -32,19 +42,13 @@ void update3D()
 	//not a real gl function and will likely change
 	glPolyFmt(POLY_ALPHA(7) | POLY_CULL_NONE);
 
-	//draw the obj
-	glBegin(GL_TRIANGLE);
-
-	glColor3b(255, 0, 0);
-	glVertex3v16(0, 0, 0);
-
-	glColor3b(0, 255, 0);
-	glVertex3v16(0, 192, 0);
-
-	glColor3b(0, 0, 255);
-	glVertex3v16(256,96, 0);
-
-	glEnd();
+	for (auto &i : triangles)
+	{
+		glBegin(GL_TRIANGLE);
+		glColor3b(0, 192, 255);
+		for (auto &j : i.elements)
+			glVertex3v16(j.first, j.second, 0);
+	}
 
 	glPopMatrix(1);
 
