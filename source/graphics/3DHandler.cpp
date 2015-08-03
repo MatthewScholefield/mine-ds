ea397445de95216540815b7ca8979890d9f66995
Wolfgange3311999 <matthew3311999@gmail.com>
2015-08-03 10:59:16 -0500

    Converted spaces to tabs

--- source/graphics/3DHandler.cpp
+++ source/graphics/3DHandler.cpp
@@ -22,29 +22,29 @@ void init3D()
 {
 	// initialize gl
 	glInit();
-  BLEND_AB = 0x1010;
-  BLEND_CR = BLEND_ALPHA | BLEND_SRC_BG0 |BLEND_DST_BG2;	
+	BLEND_AB = 0x1010;
+	BLEND_CR = BLEND_ALPHA | BLEND_SRC_BG0 | BLEND_DST_BG2;
 	// enable antialiasing
 	glEnable(GL_BLEND); //DISP3DCNT_ENUM
- 
+
 	// setup the rear plane
 	glClearColor(0, 0, 0, 0); // BG must be opaque for AA to work
-  glClearPolyID(0);
+	glClearPolyID(0);
 	glClearDepth(0x7FFF);
 	//this should work the same as the normal gl call
 	glViewport(0, 0, 255, 191);
 	//any floating point gl call is being converted to fixed prior to being implemented
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-  glOrthof32(0,256,192,0,-100000,100000);
-	glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE ); //*/
+	glOrthof32(0, 256, 192, 0, -100000, 100000);
+	glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE); //*/
 }
 
 void update3D()
 {
 	glPushMatrix();
 	glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
+	glLoadIdentity();
 	//not a real gl function and will likely change
 	glPolyFmt(POLY_ALPHA(7) | POLY_CULL_NONE);
 
