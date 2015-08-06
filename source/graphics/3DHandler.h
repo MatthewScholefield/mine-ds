f48f023c4f86425b1c0a96f3dc958c2b430e1ee5
Wolfgange3311999 <matthew3311999@gmail.com>
2015-08-05 23:49:32 -0500

    Water Mob improvements - Darkening against background - Attempt at pushing water directly into the block after touching water

--- source/graphics/3DHandler.h
+++ source/graphics/3DHandler.h
@@ -12,6 +12,11 @@ public:
 		elements[1] = b;
 		elements[2] = c;
 	}
+
+	Pair3<T> operator/(T t)
+	{
+		return Pair3(elements[0] / t, elements[1] / t, elements[2] / t);
+	}
 };
 
 void drawTriangle(Pair3<int> color, int x1, int y1, int x2, int y2, int x3, int y3);
