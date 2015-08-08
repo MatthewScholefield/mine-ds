82a0ea002d2ebd973f114f2a24889ba4c1e0e63a
Wolfgange3311999 <matthew3311999@gmail.com>
2015-08-08 03:00:08 -0500

    Some 3DHandler refactoring and WaterMob bug fixes

--- source/graphics/3DHandler.h
+++ source/graphics/3DHandler.h
@@ -1,25 +1,13 @@
 #pragma once
+#include <array>
+#include <valarray>
 
-template <class T>
-class Pair3
-{
-public:
-	T elements[3];
+using Coord = std::pair<int, int>;
+using Color = std::valarray<int>;
+using Rectangle = std::array<Coord, 4>;
+using ColoredRect = std::pair<Color, Rectangle>;
 
-	Pair3(T a, T b, T c)
-	{
-		elements[0] = a;
-		elements[1] = b;
-		elements[2] = c;
-	}
-
-	Pair3<T> operator/(T t)
-	{
-		return Pair3(elements[0] / t, elements[1] / t, elements[2] / t);
-	}
-};
-
-void drawTriangle(Pair3<int> color, int x1, int y1, int x2, int y2, int x3, int y3);
-void drawRect(Pair3<int> color, int x, int y, int sx, int sy);
+void drawTriangle(Color color, int x1, int y1, int x2, int y2, int x3, int y3);
+void drawRect(Color color, int x, int y, int sx, int sy);
 void init3D();
 void update3D();
