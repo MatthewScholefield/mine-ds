072417eecc4f8e6f8a3c1aa3986d6dc81d6bee96
Wolfgange3311999 <matthew3311999@gmail.com>
2015-08-10 23:19:20 -0500

    Added a base Interface class and folder

---
+++ source/interfaces/Interface.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <nds.h>
+#include "../world.h"
+
+enum InterfaceType
+{
+	INTERFACE_INVENTORY,
+	INTERFACE_CRAFTING,
+	INTERFACE_CHEST,
+	INTERFACE_FURNACE
+};
+
+class Interface
+{
+protected:
+
+	virtual bool closeOnMovement()
+	{
+		return true;
+	}
+public:
+	InterfaceType type;
+	virtual void update(WorldObject *world, touchPosition *touch) = 0;
+	virtual void draw() = 0;
+
+	Interface(InterfaceType type) : type(type) { }
+	virtual ~Interface();
+};
