f4b0f5ac76a5e94ab0de571f3faf976a557fe9b0
Wolfgange3311999 <matthew3311999@gmail.com>
2015-09-02 21:09:53 -0500

    WIP functional furnace

--- source/graphics/UI.h
+++ source/graphics/UI.h
@@ -9,3 +9,4 @@ int listMenu(int x, int y, int numItems, int maxNameLength);
 bool createDialog(std::string message, bool cancel = true, bool firstHalf = true, bool mineDS = true);
 void drawBackground(bool firstSlot = true, bool mineDS = true);
 void drawInvSlot(int x, int y, int tile = 154);
+bool touchesTileBox(const touchPosition &touch, int x, int y, int sx, int sy);
