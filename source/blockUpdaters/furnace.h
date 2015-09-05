a103d01d1b697af011b4ea111e481a050d34bd11
Wolfgange3311999 <matthew3311999@gmail.com>
2015-09-05 17:45:20 -0500

    Added new files

---
+++ source/blockUpdaters/furnace.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../world.h"
+#include "../blockUpdater.h"
+#include "../graphics/graphics.h"
+
+class FurnaceUpdater : public BlockUpdater
+{
+private:
+	int bufferIndex = 0;
+public:
+	FurnaceUpdater();
+	bool update(WorldObject* world, int x, int y, bool bg);
+	Graphic fireParticle;
+	Graphic clones[16];
+};
+
