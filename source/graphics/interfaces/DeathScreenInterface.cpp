976806f6a91858910f4b1cb4a68d02f49d490c81
Wolfgange3311999 <matthew3311999@gmail.com>
2015-09-12 23:00:15 -0500

    Added new files

---
+++ source/graphics/interfaces/DeathScreenInterface.cpp
@@ -0,0 +1,26 @@
+#include "DeathScreenInterface.h"
+#include "interfaceHandler.h"
+#include "../../mainGame.h"
+#include "../../mobs/mobHandler.h"
+
+void DeathScreenInterface::draw()
+{
+	clearText();
+	drawBackground();
+	printXY(11, 9, "You Died!");
+	menu.draw();
+}
+
+void DeathScreenInterface::update(WorldObject& world, touchPosition& touch)
+{
+	switch (menu.update(touch))
+	{
+	case RESPAWN:
+		triggerPlayerRespawn();
+		setInterface(INTERFACE_INVENTORY);
+		break;
+	case TITLESCREEN:
+		quitGame();
+		break;
+	}
+}
