9116d5663ed0bca740f736ed52542f30cab1f738
Wolfgange3311999 <matthew3311999@gmail.com>
2015-09-12 23:00:15 -0500

    Added new files

---
+++ source/graphics/interfaces/DeathScreenInterface.h
@@ -0,0 +1,32 @@
+#include "Interface.h"
+#include "../Menu.h"
+#include "../../mining.h"
+#include "../UI.h"
+
+#pragma once
+
+class DeathScreenInterface : public Interface
+{
+private:
+	Menu menu;
+
+	//Buttons
+	static const int RESPAWN = 1;
+	static const int TITLESCREEN = 2;
+public:
+	void update(WorldObject &world, touchPosition &touch);
+	void draw();
+
+	DeathScreenInterface() : Interface(INTERFACE_DEATH_SCREEN), menu(MENU_BUTTON, false)
+	{
+		menu.addButton(8, 10, "Respawn", 14);
+		menu.addButton(8, 15, "TitleScreen", 14);
+		setMiningDisabled(true);
+		lcdMainOnTop();
+	}
+
+	~DeathScreenInterface()
+	{
+		setMiningDisabled(false);
+	}
+};
