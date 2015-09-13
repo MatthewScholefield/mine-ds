622bc53996e78212ca24635a1e9a9f0b59f55c9a
Wolfgange3311999 <matthew3311999@gmail.com>
2015-09-12 23:11:14 -0500

    Chests are now loaded to/from file within chests.cpp

--- source/chests.h
+++ source/chests.h
@@ -5,3 +5,5 @@ void destroyChest(WorldObject &world, int x, int y, bool bg);
 void openChest(WorldObject &world, int x, int y, bool bg);
 void closeChest();
 int getOpenedChestID();
+void saveChests(FILE *file, WorldObject &world);
+void loadChests(FILE *file, WorldObject &world);
