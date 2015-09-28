c0c9871ad3c76a0f691f26ad2cf884e62e6a97e8
Wolfgange3311999 <matthew3311999@gmail.com>
2015-09-27 21:24:34 -0500

    Changed version number

--- source/general.h
+++ source/general.h
@@ -24,7 +24,7 @@ void sleepThread(unsigned int seconds);
 void printGlobalMessage(const char* s);
 #define MESSAGE_COUNT 3
 #define MESSAGE_CLEAR_DELAY 512
-#define VERSION_STRING "v0.2.1"
+#define VERSION_STRING "v0.3.0"
 
 void update_message();
 void printLocalMessage(const char* s);
