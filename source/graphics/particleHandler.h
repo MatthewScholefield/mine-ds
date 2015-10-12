74ded1a20ed665a9f94d214bd5ff190a59e431bc
CoolAs <goldenscruff@gmail.com>
2015-10-13 10:30:07 +1100

    Added extra functionality to particleHandler. Created global water splash graphic

--- source/graphics/particleHandler.h
+++ source/graphics/particleHandler.h
@@ -12,15 +12,21 @@ private:
 	FixedPoint ax;
 	FixedPoint ay;
 	int LiveTime;
+	bool KillGraphic;
 	Graphic* g;
 public:
-	Particle(FixedPoint x, FixedPoint y, FixedPoint vx, FixedPoint vy, FixedPoint ax, FixedPoint ay,int LiveTime, Graphic* g) : x(x), y(y), vx(vx), vy(vy), ax(ax), ay(ay), LiveTime(LiveTime), g(g) {};
-	Particle(float x, float y, float vx, float vy, float ax, float ay, int LiveTime, Graphic*g );
-	Particle(int x, int y, int vx, int vy, int ax, int ay, int LiveTime, Graphic* g);
+	Particle(FixedPoint x, FixedPoint y, FixedPoint vx, FixedPoint vy, FixedPoint ax, FixedPoint ay,int LiveTime, Graphic* g, bool KillGraphic = false) : x(x), y(y), vx(vx), vy(vy), ax(ax), ay(ay), LiveTime(LiveTime), g(g) {};
+	Particle(float x, float y, float vx, float vy, float ax, float ay, int LiveTime, Graphic*g , bool KillGraphic = false);
+	Particle(int x, int y, int vx, int vy, int ax, int ay, int LiveTime, Graphic* g, bool KillGraphic = false);
+	~Particle();
 	bool UpdatePhys();
 	void Draw(WorldObject& world);
 };
 
+Graphic* getCloneWaterGraphic();
+
+
 void addParticle(Particle p);
 
 void updateParticles(WorldObject& world);
+void initParticles();
