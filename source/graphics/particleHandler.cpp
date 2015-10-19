812af95a79799943b5d3574d72dda4f3471eccdb
Wolfgange3311999 <matthew3311999@gmail.com>
2015-10-18 19:26:16 -0500

    Renamed some variables

--- source/graphics/particleHandler.cpp
+++ source/graphics/particleHandler.cpp
@@ -22,30 +22,30 @@ Graphic* getCloneWaterGraphic()
 
 Particle::~Particle()
 {
-	if (KillGraphic)
+	if (killGraphic)
 		unloadGraphic(&g);
 }
 
 Particle::Particle(float x, float y, float vx, float vy, float ax
-				   , float ay, int LiveTime, Graphic g, bool KillGraphic)
+				   , float ay, int liveTime, Graphic g, bool killGraphic)
 : x(FixedPoint(true, (int) (x * FixedPoint::SCALER))), y(FixedPoint(true, (int) (y * FixedPoint::SCALER)))
 , vx(FixedPoint(true, (int) (vx * FixedPoint::SCALER))), vy(FixedPoint(true, (int) (vy * FixedPoint::SCALER)))
 , ax(FixedPoint(true, (int) (ax * FixedPoint::SCALER))), ay(FixedPoint(true, (int) (ay * FixedPoint::SCALER)))
-, LiveTime(LiveTime), KillGraphic(KillGraphic), g(g) { }
+, liveTime(liveTime), killGraphic(killGraphic), g(g) { }
 
-Particle::Particle(int x, int y, int vx, int vy, int ax, int ay, int LiveTime, Graphic g, bool KillGraphic)
-: Particle(FixedPoint(x), FixedPoint(y), FixedPoint(vx), FixedPoint(vy), FixedPoint(ax), FixedPoint(ay), LiveTime, g, KillGraphic) { }
+Particle::Particle(int x, int y, int vx, int vy, int ax, int ay, int liveTime, Graphic g, bool killGraphic)
+: Particle(FixedPoint(x), FixedPoint(y), FixedPoint(vx), FixedPoint(vy), FixedPoint(ax), FixedPoint(ay), liveTime, g, killGraphic) { }
 
-bool Particle::UpdatePhys()
+bool Particle::updatePhys()
 {
 	x += vx;
 	y += vy;
 	vx += ax;
 	vy += ay;
-	return (bool)--LiveTime;
+	return (bool)--liveTime;
 }
 
-void Particle::Draw(WorldObject& world)
+void Particle::draw(WorldObject& world)
 {
 	showGraphic(&g, x - world.camX, y - world.camY);
 }
@@ -60,9 +60,9 @@ void updateParticles(WorldObject& world)
 	std::vector<Particle>::iterator it;
 	for (it = particles.begin(); it != particles.end(); ++it)
 	{
-		bool ShouldDelete = (*it).UpdatePhys();
-		(*it).Draw(world);
-		if (!ShouldDelete)
+		bool shouldDelete = (*it).updatePhys();
+		(*it).draw(world);
+		if (!shouldDelete)
 		{
 			--it;
 			particles.erase(it + 1);
