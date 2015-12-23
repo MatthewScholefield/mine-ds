29c5ea0125c4f4f0ff3b8c2906607611b1077459
Wolfgange3311999 <matthew3311999@gmail.com>
2015-12-22 19:08:35 -0600

    Removed extra WorldObject members and comment

--- source/world.h
+++ source/world.h
@@ -4,9 +4,6 @@
 #include "Furnace.h"
 #include "inventory.h"
 
-//Do not change WORLD_HEIGHT or WORLD_WIDTH unless you are prepared to update assembly code
-//Assembly functions in worldfunctions.s depend on these values being as they are
-//Also do not change order of arrays in worldObject struct, just append things to the bottom
 #define WORLD_HEIGHT	(128)
 #define WORLD_WIDTH	(512)
 #define WORLD_HEIGHTPX	(WORLD_HEIGHT * 16)
@@ -45,11 +42,9 @@ class WorldObject
 	void initialize();
 public:
 	int blocks[WORLD_WIDTH + 1][WORLD_HEIGHT + 1];
+	int bgblocks[WORLD_WIDTH + 1][WORLD_HEIGHT + 1];
 	int data[WORLD_WIDTH + 1][WORLD_HEIGHT + 1];
 	int brightness[WORLD_WIDTH + 1][WORLD_HEIGHT + 1];
-	int lightemit[WORLD_WIDTH + 1][WORLD_HEIGHT + 1];
-	int sun[WORLD_WIDTH + 1][WORLD_HEIGHT + 1];
-	int bgblocks[WORLD_WIDTH + 1][WORLD_HEIGHT + 1];
 
 	int spawnX;
 	int camY;
