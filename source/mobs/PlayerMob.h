04dec7cc6c8929b723c0525536d2e2a8f1974d1a
Wolfgange3311999 <matthew3311999@gmail.com>
2015-12-23 22:43:21 -0600

    Controls now lock when viewing a chest

--- source/mobs/PlayerMob.h
+++ source/mobs/PlayerMob.h
@@ -7,6 +7,7 @@ class PlayerMob : public BaseMob
 {
 private:
 	Graphic mineSprite;
+	static bool controlsEnabled;
 	virtual int getMaxHealth()
 	{
 		return 20;
@@ -15,6 +16,7 @@ public:
 	bool deathScreen;
 	int tillBrightness;
 
+	static void setControlsEnabled(bool enabled);
 	void calcMiscData(WorldObject &world);
 	void saveToFile(FILE* sFile);
 	void loadFromFile(FILE* sFile);
@@ -26,6 +28,7 @@ public:
 	PlayerMob(int x, int y) : BaseMob(MOB_PLAYER, x, y, 6, 32)
 	, mineSprite(), deathScreen(false), tillBrightness(0)
 	{
+		controlsEnabled = true;
 		addInventory(CHEST);
 		addInventory(DIRT, 27);
 	}
